1702c60c92d3104588ac3ee9838baa0967fa399e
Li Haoyi <haoyi@dropbox.com>
2015-02-26 11:57:51 +0800

    String-session based tests now work, ported `AdvancedTests`

--- repl/src/test/scala/ammonite/repl/AdvancedTests.scala
+++ repl/src/test/scala/ammonite/repl/AdvancedTests.scala
@@ -10,67 +10,102 @@ object AdvancedTests extends TestSuite{
     'load{
       'ivy{
         'standalone{
-          check.fail("import scalatags.Text.all._", _.contains("not found: value scalatags"))
-          check("""load.ivy("com.lihaoyi" %% "scalatags" % "0.4.5")""")
-          check("import scalatags.Text.all._", "import scalatags.Text.all._")
-          check(
-            """a("omg", href:="www.google.com").render""",
-            """res2: String = "<a href=\"www.google.com\">omg</a>""""
-          )
+          check.session("""
+            @ import scalatags.Text.all._
+            error: not found: value scalatags
+
+            @ load.ivy("com.lihaoyi" %% "scalatags" % "0.4.5")
+
+            @ import scalatags.Text.all._
+            import scalatags.Text.all._
+
+            @ a("omg", href:="www.google.com").render
+            res2: String = "<a href=\"www.google.com\">omg</a>"
+          """)
         }
         'dependent{
           // Make sure it automatically picks up jawn-parser since upickle depends on it,
-          check("""load.ivy("com.lihaoyi", "upickle_2.11", "0.2.6")""")
-          check("import upickle._")
-          check("upickle.write(Seq(1, 2, 3))", """res2: String = "[1,2,3]"""")
+          check.session("""
+            @ load.ivy("com.lihaoyi", "upickle_2.11", "0.2.6")
+
+            @ import upickle._
+            import upickle._
+
+            @ upickle.write(Seq(1, 2, 3))
+            res2: String = "[1,2,3]"
+          """)
         }
 
         'reloading{
           // Make sure earlier-loaded things indeed continue working
-          check("""load.ivy("com.lihaoyi" %%"scalarx" % "0.2.7")""")
-          check("""load.ivy("com.scalatags" %% "scalatags" % "0.2.5")""")
-          check(
-            """scalatags.all.div("omg").toString""",
-            """res2: java.lang.String = "<div>omg</div>""""
-          )
-          check("""load.ivy("com.lihaoyi" %% "scalatags" % "0.4.5")""")
-          check(
-            """import scalatags.Text.all._; scalatags.Text.all.div("omg").toString""",
-            """import scalatags.Text.all._
-              |res4_1: java.lang.String = "<div>omg</div>"""".stripMargin
-          )
-          check("""import rx._; val x = Var(1); val y = Rx(x() + 1)""")
-          check("""x(); y()""", "res6_0: Int = 1\nres6_1: Int = 2")
-          check("""x() = 2""")
-          check("""x(); y()""", "res8_0: Int = 2\nres8_1: Int = 3")
+          check.session("""
+            @ load.ivy("com.lihaoyi" %%"scalarx" % "0.2.7")
+
+            @ load.ivy("com.scalatags" %% "scalatags" % "0.2.5")
+
+            @ scalatags.all.div("omg").toString
+            res2: java.lang.String = "<div>omg</div>"
+
+            @ load.ivy("com.lihaoyi" %% "scalatags" % "0.4.5")
+
+            @ import scalatags.Text.all._; scalatags.Text.all.div("omg").toString
+            import scalatags.Text.all._
+            res4_1: java.lang.String = "<div>omg</div>"
+
+            @ import rx._; val x = Var(1); val y = Rx(x() + 1)
+
+            @ x(); y()
+            res6_0: Int = 1
+            res6_1: Int = 2
+
+            @ x() = 2
+
+            @ x(); y()
+            res8_0: Int = 2
+            res8_1: Int = 3
+          """)
         }
         'complex{
-          check("""load.ivy("com.typesafe.akka" %% "akka-http-experimental" % "1.0-M3")""")
-          check("""implicit val system = akka.actor.ActorSystem()""")
-          check("""val serverBinding = akka.http.Http(system).bind(interface = "localhost", port = 31337)""")
-          check("""implicit val materializer = akka.stream.ActorFlowMaterializer()""")
-          check("""var set = false""")
-          check("""
-            serverBinding.connections.runForeach { connection =>
-              set = true
-            }
+          check.session("""
+            @ load.ivy("com.typesafe.akka" %% "akka-http-experimental" % "1.0-M3")
+
+            @ implicit val system = akka.actor.ActorSystem()
+
+            @ val serverBinding = akka.http.Http(system).bind(interface = "localhost", port = 31337)
+
+            @ implicit val materializer = akka.stream.ActorFlowMaterializer()
+
+            @ var set = false
+
+            @ serverBinding.connections.runForeach { connection =>
+            @   set = true
+            @ }
+
+            @ set
+            res6: Boolean = false
+
+            @ akka.stream.scaladsl.Source(
+            @   List(akka.http.model.HttpRequest(uri="/"))
+            @ ).via(
+            @   akka.http.Http().outgoingConnection("localhost", port=31337).flow
+            @ ).runForeach(println)
+
+            @ Thread.sleep(200)
+
+            @ set
+            res9: Boolean = true
+
+            @ system.shutdown()
           """)
-          check("""set""", "res6: Boolean = false")
-          check("""
-            akka.stream.scaladsl.Source(
-              List(akka.http.model.HttpRequest(uri="/"))
-            ).via(
-              akka.http.Http().outgoingConnection("localhost", port=31337).flow
-            ).runForeach(println)
-          """)
-          check("""Thread.sleep(200)""")
-          check("""system.shutdown()""")
-          check("""set""", "res10: Boolean = true")
         }
       }
       'code{
-        check("""load("val x = 1")""")
-        check("""x""", "res2: Int = 1")
+        check.session("""
+          @ load("val x = 1")
+
+          @ x
+          res2: Int = 1
+        """)
       }
     }
     'multiline{
@@ -91,9 +126,15 @@ object AdvancedTests extends TestSuite{
       check("2", "res1: Int = 2")
     }
     'history{
-      check("""val x = 1""")
-      check("x")
-      check("history", """res2: scala.Seq[String] = Vector("val x = 1", "x")""")
+      check.session("""
+        @ val x = 1
+
+        @ x
+
+        @ history
+        res2: scala.Seq[String] = Vector("val x = 1", "x")
+      """)
+
     }
   }
 }
--- repl/src/test/scala/ammonite/repl/Checker.scala
+++ repl/src/test/scala/ammonite/repl/Checker.scala
@@ -11,6 +11,24 @@ class Checker {
     Ref[String]("")
   )
 
+  def session(sess: String): Unit ={
+    val margin = sess.lines.filter(_.trim != "").map(_.takeWhile(_ == ' ').length).max
+    val steps = sess.replace("\n" + margin, "\n").split("\n\n")
+    for(step <- steps){
+      val (cmdLines, resultLines) = step.lines.map(_.drop(margin)).partition(_.startsWith("@"))
+      val commandText =
+        cmdLines.map(_.stripPrefix("@ "))
+                .mkString("\n")
+      val expected = resultLines.mkString("\n").trim
+
+      if (expected.startsWith("error: ")){
+        fail(commandText, _.contains(expected.drop("error: ".length)))
+      }else{
+        apply(commandText, if (expected == "") null else expected)
+      }
+    }
+  }
+
   def run(input: String) = {
     print(".")
     val msg = collection.mutable.Buffer.empty[String]
@@ -23,8 +41,10 @@ class Checker {
             expected: String = null) = {
     val (processed, printed) = run(input)
 
-    if (expected != null)
-      assert(printed == Result.Success(expected.trim))
+    if (expected != null){
+      val expectedRes = Result.Success(expected.trim)
+      assert(printed == expectedRes)
+    }
   }
   def fail(input: String,
            failureCheck: String => Boolean = _ => true) = {
