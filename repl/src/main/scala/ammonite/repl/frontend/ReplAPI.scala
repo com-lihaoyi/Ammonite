fc50d5ec8eed9b1e44ec236a719a71e6bff1d343
Li Haoyi <haoyi@dropbox.com>
2015-03-02 00:45:28 +0800

    documentation-is-scalatex

--- repl/src/main/scala/ammonite/repl/frontend/ReplAPI.scala
+++ repl/src/main/scala/ammonite/repl/frontend/ReplAPI.scala
@@ -43,12 +43,15 @@ trait ReplAPI {
   def history: Seq[String]
 
   /**
-   * Get the `Type` object of [[T]]
+   * Get the `Type` object of [[T]]. Useful for finding
+   * what its methods are and what you can do with it
    */
   def typeOf[T: WeakTypeTag]: Type
 
   /**
-   * Get the `Type` object representing the type of `t`
+   * Get the `Type` object representing the type of `t`. Useful
+   * for finding what its methods are and what you can do with it
+   *
    */
   def typeOf[T: WeakTypeTag](t: => T): Type
   
