594b7bc88b400ed7f66dbd9beb219f0d64b70729
Li Haoyi <haoyi@dropbox.com>
2015-06-09 21:59:50 -0700

    Bunch more navigation tests

--- terminal/src/test/scala/ammonite/terminal/NavigationTests.scala
+++ terminal/src/test/scala/ammonite/terminal/NavigationTests.scala
@@ -10,13 +10,13 @@ object NavigationTests extends TestSuite{
     val min = lines.map(_.indexWhere(_ != ' '))
       .filter(_ != -1)
       .min
-    lines.drop(1).map(_.drop(min)).mkString("\n")
+    lines.drop(1).dropRight(1).map(_.drop(min)).mkString("\n")
 
   }
   def check(grid0: String,
             start0: String,
             end0: String,
-            action: Int => Int) = {
+            actions: (Int => Int)*) = {
 
 
     val grid = normalize(grid0)
@@ -24,14 +24,23 @@ object NavigationTests extends TestSuite{
     val end = normalize(end0)
 
     val startCursor = start.indexOf('_')
-    val endCursor = action(startCursor)
-    val endState = grid.updated(endCursor, '_')
+    val endCursor = actions.foldLeft(startCursor)(
+      (a, f) => math.min(grid.length, math.max(0, f(a)))
+    )
+
+    val endState =
+      if (endCursor == grid.length) grid + '_'
+      else if (grid(endCursor) != '\n') grid.updated(endCursor, '_')
+      else{
+        val (a, b) = grid.splitAt(endCursor)
+        a + '_' + b
+      }
+
     println(startCursor + " -> " + endCursor)
     assert(end == endState)
   }
   val tests = TestSuite{
-    'test{
-
+    'simple{
       val width = 5
       val grid =
         """
@@ -39,6 +48,8 @@ object NavigationTests extends TestSuite{
         efgh
         ijkl
         """
+      val gridv = normalize(grid).toVector
+
       val start =
         """
         abcd
@@ -46,36 +57,101 @@ object NavigationTests extends TestSuite{
         ijkl
         """
 
-      def check(action: Int => Int, end: String) = {
-        NavigationTests.check(grid, start, end, action)
+      def check(end: String, actions: (Int => Int)*) = {
+        NavigationTests.check(grid, start, end, actions:_*)
       }
-
-      * - check(
-        x => x,
+      'noop - check(
         """
         abcd
         e_gh
         ijkl
-        """
+        """,
+        x => x
       )
-      * - check(
-        Term.moveDown(normalize(grid).toVector, _, width),
-        """
-        abcd
-        efgh
-        i_kl
-        """
-      )
-      * - check(
-        Term.moveUp(normalize(grid).toVector, _, width),
-        """
-        a_cd
-        efgh
-        ijkl
-        """
-      )
+      'upsAndDowns{
+
+        'down - check(
+          """
+          abcd
+          efgh
+          i_kl
+          """,
+          Term.moveDown(gridv, _, width)
+        )
+        'up - check(
+          """
+          a_cd
+          efgh
+          ijkl
+          """,
+          Term.moveUp(gridv, _, width)
+        )
+        'updown - check(
+          """
+          abcd
+          e_gh
+          ijkl
+          """,
+          Term.moveUp(gridv, _, width),
+          Term.moveDown(gridv, _, width)
+        )
+        'upup - check(
+          """
+          _bcd
+          efgh
+          ijkl
+          """,
+          Term.moveUp(gridv, _, width),
+          Term.moveUp(gridv, _, width)
+        )
+        'downdown- check(
+          """
+          abcd
+          efgh
+          ijkl_
+          """,
+          Term.moveDown(gridv, _, width),
+          Term.moveDown(gridv, _, width)
+        )
+        'upupdown - check(
+          """
+          abcd
+          _fgh
+          ijkl
+          """,
+          Term.moveUp(gridv, _, width),
+          Term.moveUp(gridv, _, width),
+          Term.moveDown(gridv, _, width)
+        )
+        'downdownup - check(
+          """
+          abcd
+          efgh_
+          ijkl
+          """,
+          Term.moveDown(gridv, _, width),
+          Term.moveDown(gridv, _, width),
+          Term.moveUp(gridv, _, width)
+        )
+      }
+      'startEnd{
+        'end - check(
+          """
+          abcd
+          efgh_
+          ijkl
+          """,
+          Term.moveStartEnd(gridv, _, 1)
+        )
+        'start - check(
+          """
+          abcd
+          _fgh
+          ijkl
+          """,
+          Term.moveStartEnd(gridv, _, 0)
+        )
+      }
     }
-
-
   }
 }
