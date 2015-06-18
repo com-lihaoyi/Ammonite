ef82d68f53c912d2971ecc7e6a6c88a121d60109
László Mérő <laci37@gmail.com>
2015-06-18 14:39:38 +0200

    fix line numbers

--- repl/src/test/scala/ammonite/repl/ScriptTests.scala
+++ repl/src/test/scala/ammonite/repl/ScriptTests.scala
@@ -51,7 +51,7 @@ object ScriptTests extends TestSuite{
 
           @ val r = res
           error: Compilation Failed
-          Main.scala:30: not found: value res
+          Main.scala:29: not found: value res
           res
           ^
           """)
@@ -62,7 +62,7 @@ object ScriptTests extends TestSuite{
 
           @ val r = res
           error: Compilation Failed
-          Main.scala:40: not found: value res
+          Main.scala:39: not found: value res
           res
           ^
 
@@ -84,7 +84,7 @@ object ScriptTests extends TestSuite{
 
           @ val r2 = res2
           error: Compilation Failed
-          Main.scala:42: not found: value res2
+          Main.scala:41: not found: value res2
           res2 
           ^
           """)
