f53d730f696868dc5ec2851552fda25c3f5cc95a
László Mérő <laci37@gmail.com>
2015-07-18 00:29:01 +0200

    copy tests for load.module

--- repl/src/test/scala/ammonite/repl/ScriptTests.scala
+++ repl/src/test/scala/ammonite/repl/ScriptTests.scala
@@ -7,93 +7,183 @@ object ScriptTests extends TestSuite{
     println("ScriptTests")
     val check = new Checker()
     val scriptPath = "repl/src/test/resource/scripts"
+    'exec{
+      'compilationBlocks{
+        'loadIvy{
+          check.session(s"""
+            @ load.exec("$scriptPath/LoadIvy.scala")
 
-    'compilationBlocks{
-      'loadIvy{
-        check.session(s"""
-          @ load.exec("$scriptPath/LoadIvy.scala")
+            @ val r = res
+            r: String = ${"\"\"\""}
+            <a href="www.google.com">omg</a>
+            ${"\"\"\""}
+            """)
+        }
+        'preserveImports{
+          val typeString =
+            if (!scala.util.Properties.versionString.contains("2.10"))
+              """Left[String, Nothing]"""
+            else
+              """util.Left[String,Nothing]"""
+          check.session(s"""
+            @ load.exec("$scriptPath/PreserveImports.scala")
 
-          @ val r = res
-          r: String = ${"\"\"\""}
-          <a href="www.google.com">omg</a>
-          ${"\"\"\""}
-          """)
+            @ val r = res
+            r: $typeString = Left("asd")
+            """)
+        }
+        'annotation{
+          check.session(s"""
+            @ load.exec("$scriptPath/Annotation.scala")
+
+            @ val r = res
+            r: Int = 24
+            """)
+        }
+        'syntax{
+          check.session(s"""
+            @ load.exec("$scriptPath/BlockSepSyntax.scala")
+
+            @ val r = res
+            r: Int = 24
+            """)
+        }
       }
-      'preserveImports{
-        val typeString =
-          if (!scala.util.Properties.versionString.contains("2.10"))
-            """Left[String, Nothing]"""
-          else
-            """util.Left[String,Nothing]"""
-        check.session(s"""
-          @ load.exec("$scriptPath/PreserveImports.scala")
+      'failures{
+        'syntaxError{
+          check.session(s"""
+            @ load.exec("$scriptPath/SyntaxError.scala")
 
-          @ val r = res
-          r: $typeString = Left("asd")
-          """)
-      }
-      'annotation{
-        check.session(s"""
-          @ load.exec("$scriptPath/Annotation.scala")
+            @ val r = res
+            error: Compilation Failed
+            Main.scala:\\d\\+: not found: value res
+            res
+            ^
+            """)
+        }
+        'compilationError{
+          check.session(s"""
+            @ load.exec("$scriptPath/CompilationError.scala")
 
-          @ val r = res
-          r: Int = 24
-          """)
-      }
-      'syntax{
-        check.session(s"""
-          @ load.exec("$scriptPath/BlockSepSyntax.scala")
+            @ val r = res
+            error: Compilation Failed
+            Main.scala:\\d\\+: not found: value res
+            res
+            ^
 
-          @ val r = res
-          r: Int = 24
-          """)
+            """)
+        }
+        'nofile{
+          check.session(s"""
+            @ load.exec("$scriptPath/notHere")
+            error: java.nio.file.NoSuchFileException: $scriptPath/notHere
+            """
+          )
+        }
+        'multiBlockError{
+          check.session(s"""
+            @ load.exec("$scriptPath/MultiBlockError.scala")
+
+            @ val r1 = res1
+            r1: Int = 1
+
+            @ val r2 = res2
+            error: Compilation Failed
+            Main.scala:\\d\\+: not found: value res2
+            res2 
+            ^
+            """)
+        }
       }
     }
-    'failures{
-      'syntaxError{
-        check.session(s"""
-          @ load.exec("$scriptPath/SyntaxError.scala")
-          error: SyntaxError
+    
+    'module{
+      'compilationBlocks{
+        'loadIvy{
+          check.session(s"""
+            @ load.module("$scriptPath/LoadIvy.scala")
 
-          @ val r = res
-          error: Compilation Failed
-          Main.scala:\\d\\+: not found: value res
-          res
-          ^
-          """)
+            @ val r = res
+            r: String = ${"\"\"\""}
+            <a href="www.google.com">omg</a>
+            ${"\"\"\""}
+            """)
+        }
+        'preserveImports{
+          val typeString =
+            if (!scala.util.Properties.versionString.contains("2.10"))
+              """Left[String, Nothing]"""
+            else
+              """util.Left[String,Nothing]"""
+          check.session(s"""
+            @ load.module("$scriptPath/PreserveImports.scala")
+
+            @ val r = res
+            r: $typeString = Left("asd")
+            """)
+        }
+        'annotation{
+          check.session(s"""
+            @ load.module("$scriptPath/Annotation.scala")
+
+            @ val r = res
+            r: Int = 24
+            """)
+        }
+        'syntax{
+          check.session(s"""
+            @ load.module("$scriptPath/BlockSepSyntax.scala")
+
+            @ val r = res
+            r: Int = 24
+            """)
+        }
       }
-      'compilationError{
-        check.session(s"""
-          @ load.exec("$scriptPath/CompilationError.scala")
+      'failures{
+        'syntaxError{
+          check.session(s"""
+            @ load.exec("$scriptPath/SyntaxError.scala")
 
-          @ val r = res
-          error: Compilation Failed
-          Main.scala:\\d\\+: not found: value res
-          res
-          ^
+            @ val r = res
+            error: Compilation Failed
+            Main.scala:\\d\\+: not found: value res
+            res
+            ^
+            """)
+        }
+        'compilationError{
+          check.session(s"""
+            @ load.module("$scriptPath/CompilationError.scala")
 
-          """)
-      }
-      'nofile{
-        check.session(s"""
-          @ load.exec("$scriptPath/notHere")
-          error: java.nio.file.NoSuchFileException: $scriptPath/notHere
-          """
-        )
-      }
-      'multiBlockError{
-        check.session(s"""
-          @ load.exec("$scriptPath/MultiBlockError.scala")
+            @ val r = res
+            error: Compilation Failed
+            Main.scala:\\d\\+: not found: value res
+            res
+            ^
 
-          @ val r1 = res1
-          r1: Int = 1
+            """)
+        }
+        'nofile{
+          check.session(s"""
+            @ load.exec("$scriptPath/notHere")
+            error: java.nio.file.NoSuchFileException: $scriptPath/notHere
+            """
+            )
+        }
+        'multiBlockError{
+          check.session(s"""
+            @ load.module("$scriptPath/MultiBlockError.scala")
 
-          @ val r2 = res2
-          error: Compilation Failed
-          Main.scala:\\d\\+: not found: value res2
-          res2 
-          ^
-          """)
+            @ val r1 = res1
+            r1: Int = 1
+
+            @ val r2 = res2
+            error: Compilation Failed
+            Main.scala:\\d\\+: not found: value res2
+            res2 
+            ^
+            """)
+        }
       }
     }
   }
