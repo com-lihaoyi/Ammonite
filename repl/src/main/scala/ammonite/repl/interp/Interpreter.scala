1c142a93fe2266478039b019f4bb43eaf2a77b34
mrmechko <rbose@cs.rochester.edu>
2015-08-10 11:41:23 -0400

    An improvement to the state of IvyThing.scala

--- repl/src/main/scala/ammonite/repl/interp/Interpreter.scala
+++ repl/src/main/scala/ammonite/repl/interp/Interpreter.scala
@@ -146,17 +146,24 @@ class Interpreter(prompt0: Ref[String],
       def ivy(coordinates: (String, String, String), verbose: Boolean = true): Unit = {
         val (groupId, artifactId, version) = coordinates
         storage().ivyCache().get((groupId, artifactId, version)) match{
-          case Some(ps) => ps.map(new java.io.File(_)).map(handleJar)
+          case Some(ps) => {
+            ps.map(new java.io.File(_)).map(handleJar)
+            Console.err.println("success")
+          }
           case None =>
-            val resolved = IvyThing.resolveArtifact(groupId, artifactId, version, if (verbose) 2 else 1)
-            storage().ivyCache() = storage().ivyCache().updated(
-              (groupId, artifactId, version),
-              resolved.map(_.getAbsolutePath).toSet
-            )
+            val resolution = IvyThing.resolveArtifact(groupId, artifactId, version, if (verbose) 2 else 1)
+            resolution map { resolved =>
+              storage().ivyCache() = storage().ivyCache().updated(
+                (groupId, artifactId, version),
+                resolved.map(_.getAbsolutePath).toSet
+              )
 
-            resolved.map(handleJar)
+              resolved.map(handleJar)
+            } match {
+              case Some(_) => Console.err.println("success")
+              case None => Console.err.println("failure")
+            }
         }
-
         init()
       }
     }
