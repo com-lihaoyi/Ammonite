8b44141babd7c9ec35680891a815388a29513091
Li Haoyi <haoyi@dropbox.com>
2015-08-14 08:22:45 +0800

    - More comments - Simplify history code more

--- terminal/src/main/scala/ammonite/terminal/ReadlineFilters.scala
+++ terminal/src/main/scala/ammonite/terminal/ReadlineFilters.scala
@@ -32,6 +32,10 @@ object ReadlineFilters {
   // Alt-Backspace  <- cut word
   // Ctrl-y     paste last cut
 
+  /**
+   * Basic readline-style navigation, using all the obscure alphabet 
+   * hotkeys rather than using arrows
+   */
   lazy val navFilter = orElseAll(
     Case(Ctrl('b'))((b, c, m) => (b, c - 1)), // <- one char
     Case(Ctrl('f'))((b, c, m) => (b, c + 1)), // -> one char
@@ -43,6 +47,10 @@ object ReadlineFilters {
     Case(Ctrl('e'))((b, c, m) => BasicFilters.moveEnd(b, c, m.width))
   )
 
+  /**
+   * All the cut-pasting logic, though for many people they simply
+   * use these shortcuts for deleting and don't use paste much at all.
+   */
   case class CutPasteFilter() extends TermCore.DelegateFilter{
     var currentCut = Vector.empty[Char]
     def cutAllLeft(b: Vector[Char], c: Int) = {
@@ -79,20 +87,17 @@ object ReadlineFilters {
       Case(Alt + "\u007f")((b, c, m) => cutWordLeft(b, c))
     )
   }
+  
 
-
-  def firstRow(cursor: Int, buffer: Vector[Char], width: Int) = {
-    cursor < width && (buffer.indexOf('\n') >= cursor || buffer.indexOf('\n') == -1)
-  }
-  def lastRow(cursor: Int, buffer: Vector[Char], width: Int) = {
-    (buffer.length - cursor) < width && (buffer.lastIndexOf('\n') < cursor || buffer.lastIndexOf('\n') == -1)
-  }
-
+  /**
+   * Provides history navigation up and down, saving the current line. 
+   */
   case class HistoryFilter(history: () => Seq[String]) extends TermCore.DelegateFilter{
     var index = -1
     var currentHistory = Vector[Char]()
 
-    def swapInHistory(b: Vector[Char], newIndex: Int, rest: LazyList[Int]): TermState = {
+    def swapInHistory(indexIncrement: Int)(b: Vector[Char], rest: LazyList[Int]): TermState = {
+      val newIndex = constrainIndex(index + indexIncrement)
       if (index == -1 && newIndex != -1) currentHistory = b
       index = newIndex
       val h = if (index == -1) currentHistory else history()(index).toVector
@@ -104,21 +109,25 @@ object ReadlineFilters {
       if (n < -1) -1 else if (max < n) max else n
     }
 
-    def previousHistory(b: Vector[Char], rest: LazyList[Int], c: Int): TermState =
-      swapInHistory(b, constrainIndex(index + 1), rest)
+    def firstRow(cursor: Int, buffer: Vector[Char], width: Int) = {
+      cursor < width && (buffer.indexOf('\n') >= cursor || buffer.indexOf('\n') == -1)
+    }
+    def lastRow(cursor: Int, buffer: Vector[Char], width: Int) = {
+      (buffer.length - cursor) < width && (buffer.lastIndexOf('\n') < cursor || buffer.lastIndexOf('\n') == -1)
+    }
 
-    def nextHistory(b: Vector[Char], rest: LazyList[Int], c: Int): TermState =
-      swapInHistory(b, constrainIndex(index - 1), rest)
+    val previousHistory = swapInHistory(1) _
+    val nextHistory = swapInHistory(-1) _
 
     def filter = {
       case TermInfo(TS(p"\u001b[A$rest", b, c), w) if firstRow(c, b, w) =>
-        previousHistory(b, rest, c)
+        previousHistory(b, rest)
       case TermInfo(TS(p"\u0010$rest", b, c), w) if lastRow(c, b, w) =>
-        previousHistory(b, rest, c)
+        previousHistory(b, rest)
       case TermInfo(TS(p"\u001b[B$rest", b, c), w) if lastRow(c, b, w) =>
-        nextHistory(b, rest, c)
+        nextHistory(b, rest)
       case TermInfo(TS(p"\u000e$rest", b, c), w) if firstRow(c, b, w) =>
-        nextHistory(b, rest, c)
+        nextHistory(b, rest)
     }
   }
 }
