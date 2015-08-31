8444bb3890afa27615b8b196a9b1b82dd6ea310d
Vyacheslav Blinov <vblinov@unison.com>
2015-08-31 17:40:57 +0300

    Ammonite sshd

---
+++ sshd/src/test/scala/ammonite/sshd/SshTestingUtils.scala
@@ -0,0 +1,160 @@
+package ammonite.sshd
+
+import java.io._
+import java.util.concurrent.TimeoutException
+
+import ammonite.ops._
+import com.jcraft.jsch.{Channel, JSch, Session, UserInfo}
+import org.scalacheck.Gen
+
+import scala.annotation.tailrec
+import scala.concurrent.{Await, Future}
+import scala.concurrent.duration._
+import scala.util.Random
+
+import scala.concurrent.ExecutionContext.Implicits.global
+
+object SshTestingUtils {
+  def alphaNumStr = Gen.listOf(Gen.alphaNumChar).map(_.mkString)
+  def nonEmptyAlphaNumStr = Gen.nonEmptyListOf(Gen.alphaNumChar).map(_.mkString)
+
+  lazy val testUsername = "test"
+  lazy val testPassword = Random.alphanumeric.take(10).mkString
+  lazy val testUser = (testUsername, testPassword)
+
+  def genCreds = for {
+    login ← nonEmptyAlphaNumStr
+    password ← alphaNumStr
+  } yield (login, password)
+
+  def genCredsPair = for {
+    creds1 ← genCreds
+    creds2 ← genCreds
+  } yield (creds1, creds2)
+
+  def nonShellChannel = Gen.alphaStr.suchThat(_ != "shell")
+
+  def genNonMatchingCredsPair = genCredsPair.suchThat(pair => pair._1 != pair._2)
+
+
+  def withTmpDirectory[T](block: Path => T):T = {
+    lazy val tmpDir = Path(Path.makeTmp)
+    try {
+      block(tmpDir)
+    } finally {
+      rm! tmpDir
+    }
+  }
+
+  def testSshServer(user: (String, String), shell: ShellSession.Server)
+                   (implicit dir: Path) = {
+    def config = SshServerConfig(port = 0,
+      username = user._1, password = user._2, ammoniteHome = dir)
+    SshServer(config, shell)
+  }
+
+  def withTestSshServer[T](user: (String, String), testShell: () => Any = () => ???)
+                          (test: org.apache.sshd.SshServer => T)
+                          (implicit dir: Path): T = {
+    val server = testSshServer(user, (_, _) => testShell.apply())
+    server.start()
+    try {
+      test(server)
+    } finally {
+      server.stop()
+    }
+  }
+
+  def sshClient(creds: (String, String), sshServer: org.apache.sshd.SshServer): Session = {
+    sshClient(creds, Option(sshServer.getHost).getOrElse("localhost"), sshServer.getPort)
+  }
+
+  def sshClient(creds: (String, String), host: String, port: Int): Session = {
+    val client = new JSch
+    val session = client.getSession(creds._1, host, port)
+    session.setPassword(creds._2)
+    session.setUserInfo(autoAcceptHostKeyUser(creds._2))
+    session
+  }
+
+  private def autoAcceptHostKeyUser(password: String) = new UserInfo {
+    override def promptPassword(message: String): Boolean = true
+    override def promptPassphrase(message: String): Boolean = false
+    override def promptYesNo(message: String): Boolean = true
+
+    override def showMessage(message: String): Unit = { /*noop*/ }
+    override def getPassword: String = password
+    override def getPassphrase: String = ???
+  }
+
+
+  /* helper class for imitating user working with ssh shell session channel */
+  class Shell private(shell: Channel) {
+    def this(sshClient: Session) = this(sshClient.openChannel("shell"))
+
+    val input = {
+      val pipeEnd = new PipedInputStream(2048)
+      val pipeStart = new PipedOutputStream(pipeEnd)
+      shell.setInputStream(pipeEnd)
+
+      new PrintStream(pipeStart, true)
+    }
+    val output = {
+      val pipeEnd = new PipedInputStream(2048)
+      val pipeStart = new PipedOutputStream(pipeEnd)
+      shell.setOutputStream(pipeStart)
+
+      new InputStreamReader(pipeEnd) {
+        private val readBuffer = new Array[Char](1024)
+
+        private def readNextChunk(): Option[String] = {
+          val chunkSize = read(readBuffer, 0, readBuffer.length)
+          if (chunkSize <= 0) None
+          else {
+            val chunk = new Array[Char](chunkSize)
+            System.arraycopy(readBuffer, 0, chunk, 0, chunkSize)
+            Some(new String(chunk))
+          }
+        }
+
+        def readAll: String = {
+          @tailrec def readMore(buffer: StringBuilder): StringBuilder = {
+            readNextChunk() match {
+              case Some(chunk) => readMore(buffer.append(chunk))
+              case None => buffer
+            }
+          }
+          readMore(new StringBuilder).toString()
+        }
+      }
+    }
+
+    def connect() = shell.connect()
+    def isConnected = shell.isConnected
+    def disconnect() = shell.disconnect()
+
+    def awaitToBecomeDisconnected(duration: FiniteDuration) = {
+      Await.result(becomeDisconnected(duration), duration)
+    }
+
+    // there is no callback that ssh client finished channel
+    // due to ssh close session signal
+    // so we wait for it to happen in bg thread
+    def becomeDisconnected(duration: Duration): Future[Unit] = Future {
+      def currentTime = System.currentTimeMillis()
+      val startTime = currentTime
+      def elapsedTime = (currentTime - startTime).millis
+      @tailrec def awaitImpl():Unit = if (elapsedTime < duration) {
+        if (isConnected) {
+          Thread.sleep(100)
+          awaitImpl()
+        } else {
+          /* exit loop */
+        }
+      } else {
+        throw new TimeoutException("Timed out while waiting for channel to disconnect")
+      }
+      awaitImpl()
+    }
+  }
+}
---
+++ sshd/src/test/scala/ammonite/sshd/SshdReplTest.scala
@@ -0,0 +1,58 @@
+package ammonite.sshd
+
+import utest._
+import utest.framework.TestSuite
+
+import scala.concurrent.{Await, Promise}
+import scala.language.postfixOps
+import scala.concurrent.duration._
+import SshTestingUtils._
+
+object SshdReplTest extends TestSuite {
+  val remotePromise = Promise[Boolean]()
+
+  override val tests = TestSuite {
+    'canExecuteRemoteCommand{
+      withTmpDirectory { tmpDir =>
+        val repl = new SshdRepl(SshServerConfig(0, testUsername, testPassword, tmpDir))
+        repl.start()
+        val client = sshClient((testUsername, testPassword), "localhost", repl.port)
+        client.connect()
+        assert(client.isConnected)
+
+        val shell = new Shell(client)
+        shell.connect()
+        assert(shell.isConnected)
+
+        shell.input.println("2 + 2")
+        shell.input.println("import ammonite.sshd.SshdReplTest")
+        shell.input.println("SshdReplTest.remotePromise.success(true)")
+        shell.input.println("exit")
+
+        assert(Await.result(remotePromise.future, 30 seconds))
+        shell.awaitToBecomeDisconnected(60 seconds)
+
+        val outputLines = shell.output.readAll.lines.toList
+        val Seq(firstBannerLine, secondBannerLine) = outputLines.take(2)
+        val Seq(mathTestPrompt, mathOutput) = outputLines.slice(2, 4)
+        val Seq(importAppPackage, importResult) = outputLines.slice(4, 6)
+
+        assert(
+          firstBannerLine.startsWith("Welcome to the Ammonite Repl"),
+          secondBannerLine.contains("Scala"),
+          secondBannerLine.contains("Java")
+        )
+        assert(
+          mathTestPrompt.contains("2"),
+          mathTestPrompt.contains("+"),
+          mathOutput.contains("4")
+        )
+        assert(
+          importAppPackage.contains("import"),
+          importAppPackage.contains("ammonite.sshd.SshdReplTest"),
+          !importResult.toLowerCase.contains("error")
+        )
+      }
+    }
+  }
+}
