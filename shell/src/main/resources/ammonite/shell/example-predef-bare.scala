67598baec5872a723613b6327cf95abefccb4709
Li Haoyi <haoyi@dropbox.com>
2015-11-23 17:14:34 +0800

    rename sess to shellSession in examplePredef ot avoid conflict with new sess builtin

--- shell/src/main/resources/ammonite/shell/example-predef-bare.scala
+++ shell/src/main/resources/ammonite/shell/example-predef-bare.scala
@@ -3,8 +3,8 @@ val scalaVersion = scala.util.Properties.versionNumberString.split("\\.").dropRi
 val ammVersion = ammonite.Constants.version
 load.jar(cwd/'shell/'target/s"scala-$scalaVersion"/s"ammonite-shell_$scalaVersion-$ammVersion.jar")
 @
-val sess = ammonite.shell.ShellSession()
-import sess._
+val shellSession = ammonite.shell.ShellSession()
+import shellSession._
 import ammonite.ops._
 import ammonite.shell.PPrints._
 ammonite.shell.Configure(repl, wd)
--- shell/src/main/resources/ammonite/shell/example-predef.scala
+++ shell/src/main/resources/ammonite/shell/example-predef.scala
@@ -1,7 +1,7 @@
 load.ivy("com.lihaoyi" %% "ammonite-shell" % ammonite.Constants.version)
 @
-val sess = ammonite.shell.ShellSession()
-import sess._
+val shellSession = ammonite.shell.ShellSession()
+import shellSession._
 import ammonite.shell.PPrints._
 import ammonite.ops._
 ammonite.shell.Configure(repl, wd)
