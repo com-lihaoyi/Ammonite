726bd88a858b425469f14f01f6cbb866a94fadd6
Li Haoyi <haoyi@dropbox.com>
2015-12-08 00:13:53 -0800

    migrate all CommandResult examples to use apply syntax

--- ops/src/test/scala/test/ammonite/ops/ShelloutTests.scala
+++ ops/src/test/scala/test/ammonite/ops/ShelloutTests.scala
@@ -10,36 +10,36 @@ object ShelloutTests extends TestSuite{
     'implicitWd{
       import ammonite.ops.ImplicitWd._
       'lines{
-        val res = %%ls "ops/src/test/resources/testdata"
+        val res = %%('ls, "ops/src/test/resources/testdata")
         assert(res.out.lines == Seq("File.txt", "folder1", "folder2"))
       }
       'string{
-        val res = %%ls "ops/src/test/resources/testdata"
+        val res = %%('ls, "ops/src/test/resources/testdata")
         assert(res.out.string == "File.txt\nfolder1\nfolder2\n")
       }
       'bytes{
-        val res = %%echo "abc"
+        val res = %%('echo, "abc")
         val listed = res.out.bytes
 //        assert(listed == "File.txt\nfolder\nfolder2\nFile.txt".getBytes)
         listed.toSeq
       }
       'chained{
-        assert((%%git 'init).out.string.contains("Reinitialized existing Git repository"))
-        assert((%%git "init").out.string.contains("Reinitialized existing Git repository"))
-        assert((%%ls cwd).out.string.contains("readme.md"))
+        assert(%%('git, 'init).out.string.contains("Reinitialized existing Git repository"))
+        assert(%%('git, "init").out.string.contains("Reinitialized existing Git repository"))
+        assert(%%('ls, cwd).out.string.contains("readme.md"))
       }
       'basicList{
         val files = List("readme.md", "build.sbt")
-        val output = (%%ls files).out.string
+        val output = %%('ls, files).out.string
         assert(files.forall(output.contains))
       }
       'listMixAndMatch{
         val stuff = List("I", "am", "bovine")
-        val result = %%echo("Hello,", stuff, "hear me roar")
+        val result = %%('echo, "Hello,", stuff, "hear me roar")
         assert(result.out.string.contains("Hello, " + stuff.mkString(" ") + " hear me roar"))
       }
       'failures{
-        val ex = intercept[ShelloutException]{ %%ls "does-not-exist" }
+        val ex = intercept[ShelloutException]{ %%('ls, "does-not-exist") }
         val res: CommandResult = ex.result
         assert(
           res.exitCode != 0,
@@ -56,7 +56,7 @@ object ShelloutTests extends TestSuite{
         assert(res.out.string.trim == "Hello123")
       }
       'filebased2{
-        val res = %%which 'echo
+        val res = %%('which, 'echo)
         val echoRoot = Path(res.out.string.trim)
         assert(echoRoot == root/'bin/'echo)
 
@@ -64,27 +64,27 @@ object ShelloutTests extends TestSuite{
       }
 
       'envArgs{
-        val res0 = %%bash("-c", "echo \"Hello$ENV_ARG\"", ENV_ARG=12)
+        val res0 = %%('bash, "-c", "echo \"Hello$ENV_ARG\"", ENV_ARG=12)
         assert(res0.out.lines == Seq("Hello12"))
 
-        val res1 = %%bash("-c", "echo \"Hello$ENV_ARG\"", ENV_ARG=12)
+        val res1 = %%('bash, "-c", "echo \"Hello$ENV_ARG\"", ENV_ARG=12)
         assert(res1.out.lines == Seq("Hello12"))
 
-        val res2 = %%bash("-c", "echo 'Hello$ENV_ARG'", ENV_ARG=12)
+        val res2 = %%('bash, "-c", "echo 'Hello$ENV_ARG'", ENV_ARG=12)
         assert(res2.out.lines == Seq("Hello$ENV_ARG"))
 
-        val res3 = %%bash("-c", "echo 'Hello'$ENV_ARG", ENV_ARG=123)
+        val res3 = %%('bash, "-c", "echo 'Hello'$ENV_ARG", ENV_ARG=123)
         assert(res3.out.lines == Seq("Hello123"))
       }
 
     }
     'workingDirectory{
       implicit var wd = cwd
-      val listed1 = %%ls
+      val listed1 = %%('ls)
 
       wd /= up
 
-      val listed2 = %%ls
+      val listed2 = %%('ls)
 
       assert(listed2 != listed1)
     }
