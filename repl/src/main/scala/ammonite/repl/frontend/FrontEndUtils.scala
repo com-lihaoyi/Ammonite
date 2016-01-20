235f77c97d48ae509b0269801ce214d3a1a529f7
coderabhishek <abhishekkmr18@gmail.com>
2016-01-20 23:33:03 +0530

    Protect FronntEndUtils.tabulate function from processing empty snippet sequence

--- repl/src/main/scala/ammonite/repl/frontend/FrontEndUtils.scala
+++ repl/src/main/scala/ammonite/repl/frontend/FrontEndUtils.scala
@@ -12,8 +12,9 @@ import scala.annotation.tailrec
 object FrontEndUtils {
   def width = ammonite.terminal.TTY.consoleDim("cols")
   def height = ammonite.terminal.TTY.consoleDim("lines")
-  def tabulate(snippets: Seq[String], width: Int) = {
+  def tabulate(snippets_raw: Seq[String], width: Int) = {
     val gap = 2
+    val snippets=(if (snippets_raw.isEmpty) Seq("") else (snippets_raw))
     val maxLength = snippets.maxBy(_.replaceAll("\u001B\\[[;\\d]*m", "").length).length + gap
     val columns = math.max(1, width / maxLength)
 
