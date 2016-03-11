e4fc5adac1cbc8122d79d09dc044a78c1196f0d9
Li Haoyi <haoyi@dropbox.com>
2016-03-12 01:03:23 +0800

    upgrade to utest 0.4.1, make use of retry to try and tame flaky ivy tests

--- repl/src/test/scala/ammonite/repl/TestRepl.scala
+++ repl/src/test/scala/ammonite/repl/TestRepl.scala
@@ -148,7 +148,7 @@ class TestRepl {
   }
   def failLoudly[T](t: => T) =
     try t
-    catch{ case e: utest.AssertionError =>
+    catch{ case e: utest.framework.AssertionError =>
       println("FAILURE TRACE\n" + allOutput)
       throw e
     }
