a8f4ae2c2d3ccd3e01cfc9277934c0e6c87f6fab
coderabhishek <abhishekkmr18@gmail.com>
2016-03-12 20:59:05 +0530

    Fix indentation errors

--- integration/src/test/resources/ammonite/integration/qs.scala
+++ integration/src/test/resources/ammonite/integration/qs.scala
@@ -1,13 +1,13 @@
-def fib(n:Int):Int = {
-		if (n == 0) return 0
-		if (n == 1) return 1
-		return fib(n-1) + fib(n-2)
-	}
+def fib(n : Int) : Int = {
+  if (n == 0) 0
+  else if (n == 1) 1
+  else fib(n-1) + fib(n-2)
+}
 
-def qs(unsorted:List[Int]):List[Int]={
-		if(unsorted.length<=1) unsorted
-		else{
-		val pivot=unsorted.head
-		return qs(unsorted.filter(_ < pivot)):::List(pivot):::qs(unsorted.filter(_ > pivot))
-		}
-	}
+def quicksort(unsorted : List[Int]) : List[Int] = {
+  if(unsorted.length <= 1) unsorted
+  else{
+    val pivot = unsorted.head
+    quicksort(unsorted.filter(_ < pivot)):::List(pivot):::quicksort(unsorted.filter(_ > pivot))
+  }
+}
