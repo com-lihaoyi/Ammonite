0f584c69b29a96dd3f9526c49dd2e72dc48d8a37
Li Haoyi <haoyi@dropbox.com>
2016-06-14 20:02:18 +0800

    0.6.1

--- project/Constants.scala
+++ project/Constants.scala
@@ -1,5 +1,5 @@
 package ammonite
 
 object Constants{
-  val version = "0.6.1-SNAPSHOT"
+  val version = "0.6.1"
 }
--- readme/Sample.scala
+++ readme/Sample.scala
@@ -9,9 +9,9 @@ import scalatags.Text.all._
 import collection.mutable
 
 object Sample{
-  val replCurl = "$ curl -L -o amm https://git.io/voU0F && chmod +x amm && ./amm"
+  val replCurl = "$ curl -L -o amm https://git.io/voluT && chmod +x amm && ./amm"
   val filesystemCurl =
-    "$ mkdir ~/.ammonite && curl -L -o ~/.ammonite/predef.scala https://git.io/voU0p"
+    "$ mkdir ~/.ammonite && curl -L -o ~/.ammonite/predef.scala https://git.io/voluk"
   val cacheVersion = 6
   def cached(key: Any)(calc: => String) = {
     val path = cwd/'target/'cache/(key.hashCode + cacheVersion).toString
