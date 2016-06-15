122e810185726904f21d48dced72c6674331c272
Li Haoyi <haoyi@dropbox.com>
2016-06-15 12:28:01 +0800

    stub objects in dummy packages

---
+++ repl/src/main/scala/Stubs.scala
@@ -0,0 +1,9 @@
+// Dummy packages just to populate the existence of these top-level synthetic
+// packages in the Scala compiler.
+
+/**
+  * Package that gets filled with any script files that the user imports
+  */
+package $file{
+  private object `_`
+}
