1448942b1ab5f628c593ea6916e1a89a76cb9b6c
Alexandre Archambault <alexandre.archambault@gmail.com>
2016-07-18 20:38:17 +0200

    Move things around 2 / 12
    Remove dependency towards fastparse from Scripts

--- amm/src/main/scala/ammonite/main/Scripts.scala
+++ amm/src/main/scala/ammonite/main/Scripts.scala
@@ -5,13 +5,45 @@ import ammonite.main.Router.{ArgSig, EntryPoint}
 import ammonite.ops._
 import ammonite.util.Name.backtickWrap
 import ammonite.util.{Name, Res, Util}
-import fastparse.Utils._
+
+import scala.annotation.switch
 
 /**
   * Logic around using Ammonite as a script-runner; invoking scripts via the
   * macro-generated [[Router]], and pretty-printing any output or error messages
   */
 object Scripts {
+
+
+  /**
+    * Convert a string to a C&P-able literal. Basically
+    * copied verbatim from the uPickle source code.
+    */
+  def literalize(s: String, unicode: Boolean = true) = {
+    val sb = new StringBuilder
+    sb.append('"')
+    var i = 0
+    val len = s.length
+    while (i < len) {
+      (s.charAt(i): @switch) match {
+        case '"' => sb.append("\\\"")
+        case '\\' => sb.append("\\\\")
+        case '\b' => sb.append("\\b")
+        case '\f' => sb.append("\\f")
+        case '\n' => sb.append("\\n")
+        case '\r' => sb.append("\\r")
+        case '\t' => sb.append("\\t")
+        case c =>
+          if (c < ' ' || (c > '~' && unicode)) sb.append("\\u%04x" format c.toInt)
+          else sb.append(c)
+      }
+      i += 1
+    }
+    sb.append('"')
+
+  }
+
+
   def runScript(wd: Path,
                 path: Path,
                 repl: ammonite.main.Repl,
