2fa16af4fb477bd75bc2b2740aa8aeb3872c54c0
Harshad Deo <subterranean.hominid@gmail.com>
2016-09-07 21:27:08 +0530

    scalatest conversion - script tests done

--- src/test/scala/ammonite/ScriptTests.scala
+++ src/test/scala/ammonite/ScriptTests.scala
@@ -1,317 +1,306 @@
-// package ammonite
+package ammonite
 
-// import ammonite.TestUtils._
-// import ammonite.ops._
-// import ammonite.main.Defaults
-// import utest._
-// import ammonite.runtime.{Storage}
+import ammonite.TestUtils._
+import ammonite.ops._
+import ammonite.main.Defaults
+import ammonite.runtime.{Storage}
+import org.scalatest.FreeSpec
 
-// object ScriptTests extends TestSuite {
-//   val tests = TestSuite {
-//     println("ScriptTests")
-//     val check = new TestRepl()
+class ScriptTests extends FreeSpec {
 
-//     val printedScriptPath = """pwd/'src/'test/'resources/'scripts"""
+  def check = new TestRepl()
 
-//     'exec {
-//       'compilationBlocks {
-//         'loadIvy - retry(3) { // ivy or maven central seems to be flaky =/ =/ =/
-//           check.session(s"""
-//             @ import ammonite.ops._
+  val printedScriptPath = """pwd/'src/'test/'resources/'scripts"""
 
-//             @ interp.load.exec($printedScriptPath/"LoadIvy.sc")
+  "exec" - {
+    "compilationBlocks" - {
+      "loadIvy" in { // ivy or maven central seems to be flaky =/ =/ =/
+        check.session(s"""
+            @ import ammonite.ops._
 
-//             @ val r = res
-//             r: String = ${"\"\"\""}
-//             <a href="www.google.com">omg</a>
-//             ${"\"\"\""}
-//             """)
-//         }
-//         'preserveImports {
-//           val typeString =
-//             if (!scala2_10)
-//               """Left[String, Nothing]"""
-//             else
-//               """util.Left[String,Nothing]"""
-//           check.session(s"""
-//             @ import ammonite.ops._
+            @ interp.load.exec($printedScriptPath/"LoadIvy.sc")
 
-//             @ interp.load.exec($printedScriptPath/"PreserveImports.sc")
+            @ val r = res
+            r: String = ${"\"\"\""}
+            <a href="www.google.com">omg</a>
+            ${"\"\"\""}
+            """)
+      }
+      "preserveImports" in {
+        val typeString = """Left[String, Nothing]"""
+        check.session(s"""
+            @ import ammonite.ops._
 
-//             @ val r = res
-//             r: $typeString = Left("asd")
-//             """)
-//         }
-//         'annotation {
-//           check.session(s"""
-//             @ import ammonite.ops._
+            @ interp.load.exec($printedScriptPath/"PreserveImports.sc")
 
-//             @ interp.load.exec($printedScriptPath/"Annotation.sc")
+            @ val r = res
+            r: $typeString = Left("asd")
+            """)
+      }
+      "annotation" in {
+        check.session(s"""
+            @ import ammonite.ops._
 
-//             @ val r = res
-//             r: Int = 24
-//             """)
-//         }
-//         'syntax {
-//           check.session(s"""
-//             @ import ammonite.ops._
+            @ interp.load.exec($printedScriptPath/"Annotation.sc")
 
-//             @ interp.load.exec($printedScriptPath/"BlockSepSyntax.sc")
+            @ val r = res
+            r: Int = 24
+            """)
+      }
+      "syntax" in {
+        check.session(s"""
+            @ import ammonite.ops._
 
-//             @ val r = res
-//             r: Int = 24
-//             """)
-//         }
-//         'limitImports {
-//           check.session(s"""
-//             @ import ammonite.ops._
+            @ interp.load.exec($printedScriptPath/"BlockSepSyntax.sc")
 
-//             @ interp.load.exec($printedScriptPath/"LimitImports.sc")
+            @ val r = res
+            r: Int = 24
+            """)
+      }
+      "limitImport" in {
+        check.session(s"""
+            @ import ammonite.ops._
 
-//             @ res
-//             error: not found: value res
-//             """)
-//         }
-//       }
-//       'failures {
-//         'syntaxError {
-//           check.session(s"""
-//             @ import ammonite.ops._
+            @ interp.load.exec($printedScriptPath/"LimitImports.sc")
 
-//             @ interp.load.exec($printedScriptPath/"SyntaxError.sc")
-//             error: CompilationError
+            @ res
+            error: not found: value res
+            """)
+      }
+    }
+    "failures" - {
+      "syntaxError" in {
+        check.session(s"""
+            @ import ammonite.ops._
 
-//             @ val r = res
-//             error: not found: value res
-//             val r = res
-//                     ^
-//             Compilation Failed
-//             """)
-//         }
-//         'compilationError {
-//           check.session(s"""
-//             @  import ammonite.ops._
+            @ interp.load.exec($printedScriptPath/"SyntaxError.sc")
+            error: CompilationError
 
-//             @ interp.load.exec($printedScriptPath/"CompilationError.sc")
-//             error: Compilation Failed
+            @ val r = res
+            error: not found: value res
+            val r = res
+                    ^
+            Compilation Failed
+            """)
+      }
+      "compilationError" in {
+        check.session(s"""
+            @  import ammonite.ops._
 
-//             @ val r = res
-//             error: not found: value res
-//             val r = res
-//                     ^
-//             Compilation Failed
-//             """)
-//         }
-//         'nofile {
-//           check.session(s"""
-//             @ import ammonite.ops._
+            @ interp.load.exec($printedScriptPath/"CompilationError.sc")
+            error: Compilation Failed
 
-//             @ interp.load.exec($printedScriptPath/"notHere")
-//             error: java.nio.file.NoSuchFileException
-//             """)
-//         }
-//         'multiBlockError {
-//           check.session(s"""
-//             @ import ammonite.ops._
+            @ val r = res
+            error: not found: value res
+            val r = res
+                    ^
+            Compilation Failed
+            """)
+      }
+      "nofile" in {
+        check.session(s"""
+            @ import ammonite.ops._
 
-//             @ interp.load.exec($printedScriptPath/"MultiBlockError.sc")
-//             error: Compilation Failed
+            @ interp.load.exec($printedScriptPath/"notHere")
+            error: java.nio.file.NoSuchFileException
+            """)
+      }
+      "multiBlockError" in {
+        check.session(s"""
+            @ import ammonite.ops._
 
-//             @ val r2 = res2
-//             error: not found: value res2
-//             val r2 = res2
-//                      ^
-//             Compilation Failed
-//             """)
-//         }
-//       }
-//       'nestedScripts {
-//         check.session(s"""
-//           @ import ammonite.ops._
+            @ interp.load.exec($printedScriptPath/"MultiBlockError.sc")
+            error: Compilation Failed
 
-//           @ interp.load.exec($printedScriptPath/"NestedScripts.sc")
+            @ val r2 = res2
+            error: not found: value res2
+            val r2 = res2
+                     ^
+            Compilation Failed
+            """)
+      }
+    }
+    "nestedScripts" in {
+      check.session(s"""
+          @ import ammonite.ops._
 
-//           @ val a = asd
-//           error: not found: value asd
+          @ interp.load.exec($printedScriptPath/"NestedScripts.sc")
 
-//           @ val b = asd2
-//           b: Int = 1
-//           """)
-//       }
-//       'sheBang {
-//         check.session(s"""
-//             @  import ammonite.ops._
+          @ val a = asd
+          error: not found: value asd
 
-//             @ interp.load.exec($printedScriptPath/"SheBang.sc")
+          @ val b = asd2
+          b: Int = 1
+          """)
+    }
+    "sheBang" in {
+      check.session(s"""
+            @  import ammonite.ops._
 
-//             @ val r = res
-//             r: Int = 42
-//             """)
-//       }
+            @ interp.load.exec($printedScriptPath/"SheBang.sc")
 
-//     }
+            @ val r = res
+            r: Int = 42
+            """)
+    }
 
-//     'module {
-//       'compilationBlocks {
-//         'loadIvy {
-//           check.session(s"""
-//             @ import ammonite.ops._
+  }
 
-//             @ interp.load.module($printedScriptPath/"LoadIvy.sc")
+  "module" - {
+    "compilationBlocks" - {
+      "loadIvy" in {
+        check.session(s"""
+            @ import ammonite.ops._
 
-//             @ val r = res
-//             r: String = ${"\"\"\""}
-//             <a href="www.google.com">omg</a>
-//             ${"\"\"\""}
-//            """)
-//         }
-//         'preserveImports {
-//           val typeString =
-//             if (!scala2_10)
-//               """Left[String, Nothing]"""
-//             else
-//               """util.Left[String,Nothing]"""
-//           check.session(s"""
-//               @ import ammonite.ops._
+            @ interp.load.module($printedScriptPath/"LoadIvy.sc")
 
-//               @ interp.load.module($printedScriptPath/"PreserveImports.sc")
+            @ val r = res
+            r: String = ${"\"\"\""}
+            <a href="www.google.com">omg</a>
+            ${"\"\"\""}
+           """)
+      }
+      "preserveImports" in {
+        val typeString = """Left[String, Nothing]"""
+        check.session(s"""
+              @ import ammonite.ops._
 
-//               @ val r = res
-//               r: $typeString = Left("asd")
-//               """)
+              @ interp.load.module($printedScriptPath/"PreserveImports.sc")
 
-//         }
-//         'annotation {
-//           if (!scala2_10) //buggy in 2.10
-//             check.session(s"""
-//             @ import ammonite.ops._
+              @ val r = res
+              r: $typeString = Left("asd")
+              """)
+      }
+      "annotation" in {
+        check.session(s"""
+            @ import ammonite.ops._
 
-//             @ interp.load.module($printedScriptPath/"Annotation.sc")
+            @ interp.load.module($printedScriptPath/"Annotation.sc")
 
-//             @ val r = res
-//             r: Int = 24
-//             """)
-//         }
-//         'syntax {
-//           check.session(s"""
-//               @ import ammonite.ops._
+            @ val r = res
+            r: Int = 24
+            """)
+      }
+      "syntax" in {
+        check.session(s"""
+              @ import ammonite.ops._
 
-//               @ interp.load.module($printedScriptPath/"BlockSepSyntax.sc")
+              @ interp.load.module($printedScriptPath/"BlockSepSyntax.sc")
 
-//               @ val r = res
-//               r: Int = 24
-//             """)
-//         }
-//         'limitImports {
-//           check.session(s"""
-//             @ import ammonite.ops._
+              @ val r = res
+              r: Int = 24
+            """)
+      }
+      "limitImports" in {
+        check.session(s"""
+            @ import ammonite.ops._
 
-//             @ interp.load.module($printedScriptPath/"LimitImports.sc")
+            @ interp.load.module($printedScriptPath/"LimitImports.sc")
 
-//             @ res
-//             error: not found: value res
-//             """)
-//         }
-//       }
-//       'failures {
-//         'syntaxError {
-//           check.session(s"""
-//             @ import ammonite.ops._
+            @ res
+            error: not found: value res
+            """)
+      }
+    }
+    "failures" - {
+      "syntaxError" in {
+        check.session(s"""
+            @ import ammonite.ops._
 
-//             @ interp.load.exec($printedScriptPath/"SyntaxError.sc")
-//             error: CompilationError
+            @ interp.load.exec($printedScriptPath/"SyntaxError.sc")
+            error: CompilationError
 
-//             @ val r = res
-//             error: not found: value res
-//             val r = res
-//                     ^
-//             Compilation Failed
-//             """)
-//         }
-//         'compilationError {
-//           check.session(s"""
-//             @ import ammonite.ops._
+            @ val r = res
+            error: not found: value res
+            val r = res
+                    ^
+            Compilation Failed
+            """)
+      }
+      "compilationError" in {
+        check.session(s"""
+            @ import ammonite.ops._
 
-//             @ interp.load.module($printedScriptPath/"CompilationError.sc")
-//             error: Compilation Failed
+            @ interp.load.module($printedScriptPath/"CompilationError.sc")
+            error: Compilation Failed
 
-//             @ val r = res
-//             error: not found: value res
-//             val r = res
-//                     ^
-//             Compilation Failed""")
-//         }
-//         'nofile {
-//           check.session(s"""
-//             @ import ammonite.ops._
+            @ val r = res
+            error: not found: value res
+            val r = res
+                    ^
+            Compilation Failed""")
+      }
+      "nofile" in {
+        check.session(s"""
+            @ import ammonite.ops._
 
-//             @ interp.load.exec($printedScriptPath/"notHere")
-//             error: java.nio.file.NoSuchFileException
-//             """)
-//         }
-//         'scriptWithoutExtension {
-//           val res = intercept[java.nio.file.NoSuchFileException] {
-//             val storage =
-//               new Storage.Folder(tmp.dir(prefix = "ammonite-tester"))
-//             val interp2 = createTestInterp(
-//               storage,
-//               Defaults.predefString
-//             )
-//             interp2.interpApi.load.module(pwd / "scriptWithoutExtension")
-//           }.toString
-//           assert(res.contains("java.nio.file.NoSuchFileException"))
-//         }
-//         'multiBlockError {
-//           check.session(s"""
-//             @ import ammonite.ops._
+            @ interp.load.exec($printedScriptPath/"notHere")
+            error: java.nio.file.NoSuchFileException
+            """)
+      }
+      "scriptWithoutExtension" in {
+        val res = intercept[java.nio.file.NoSuchFileException] {
+          val storage =
+            new Storage.Folder(tmp.dir(prefix = "ammonite-tester"))
+          val interp2 = createTestInterp(
+            storage,
+            Defaults.predefString
+          )
+          interp2.interpApi.load.module(pwd / "scriptWithoutExtension")
+        }.toString
+        assert(res.contains("java.nio.file.NoSuchFileException"))
+      }
+      "multiBlockError" in {
+        check.session(s"""
+            @ import ammonite.ops._
 
-//             @ interp.load.module($printedScriptPath/"MultiBlockError.sc")
-//             error: Compilation Failed
+            @ interp.load.module($printedScriptPath/"MultiBlockError.sc")
+            error: Compilation Failed
 
-//             @ val r2 = res2
-//             error: not found: value res2
-//             val r2 = res2
-//                      ^
-//             Compilation Failed
-//             """)
-//         }
-//       }
-//       'encapsulation {
-//         check.session(s"""
-//             @ import ammonite.ops._
+            @ val r2 = res2
+            error: not found: value res2
+            val r2 = res2
+                     ^
+            Compilation Failed
+            """)
+      }
+    }
+    "encapsulation" in {
+      check.session(s"""
+            @ import ammonite.ops._
 
-//             @ val asd = "asd"
+            @ val asd = "asd"
 
-//             @ interp.load.module($printedScriptPath/"Encapsulation.sc")
-//             error: not found: value asd
-//             """)
-//       }
-//       'nestedScripts {
-//         check.session(s"""
-//           @ import ammonite.ops._
+            @ interp.load.module($printedScriptPath/"Encapsulation.sc")
+            error: not found: value asd
+            """)
+    }
+    "nestedScripts" in {
+      check.session(s"""
+          @ import ammonite.ops._
 
-//           @ interp.load.module($printedScriptPath/"NestedScripts.sc")
+          @ interp.load.module($printedScriptPath/"NestedScripts.sc")
 
-//           @ val a = asd
-//           error: not found: value asd
+          @ val a = asd
+          error: not found: value asd
 
-//           @ val b = asd2
-//           b: Int = 1
-//           """)
-//       }
-//       'noUnWrapping {
-//         check.session(s"""
-//         @ import ammonite.ops._
+          @ val b = asd2
+          b: Int = 1
+          """)
+    }
+    "noUnWrapping" in {
+      check.session(s"""
+        @ import ammonite.ops._
 
-//         @ interp.load.module($printedScriptPath/"ScriptDontUnwrap.sc")
+        @ interp.load.module($printedScriptPath/"ScriptDontUnwrap.sc")
 
-//         @ foo
-//         res2: String = "foo def"
+        @ foo
+        res2: String = "foo def"
 
-//         @ wrappedValue
-//         error: not found: value wrappedValue
-//         """)
-//       }
-//     }
-//   }
-// }
+        @ wrappedValue
+        error: not found: value wrappedValue
+        """)
+    }
+  }
+
+}
