2281b11787c5040c6613d6ae469ba0927dfc2663
Harshad Deo <subterranean.hominid@gmail.com>
2016-09-07 21:40:04 +0530

    scalatest conversion - advanced tests done

--- src/test/scala/ammonite/session/AdvancedTests.scala
+++ src/test/scala/ammonite/session/AdvancedTests.scala
@@ -1,305 +1,314 @@
-// package ammonite.session
+package ammonite.session
 
-// import ammonite.TestUtils._
-// import ammonite.TestRepl
-// import ammonite.util.Res
-// import utest._
+import ammonite.TestUtils._
+import ammonite.TestRepl
+import ammonite.util.Res
+import org.scalatest.FreeSpec
 
-// import ammonite.ops._
+import ammonite.ops._
 
-// object AdvancedTests extends TestSuite {
-//   val tests = TestSuite {
-//     println("AdvancedTests")
-//     val check = new TestRepl()
-//     'pprint {
-//       check.session(s"""
-//         @ Seq.fill(10)(Seq.fill(3)("Foo"))
-//         res0: Seq[Seq[String]] = List(
-//           List("Foo", "Foo", "Foo"),
-//           List("Foo", "Foo", "Foo"),
-//           List("Foo", "Foo", "Foo"),
-//           List("Foo", "Foo", "Foo"),
-//           List("Foo", "Foo", "Foo"),
-//           List("Foo", "Foo", "Foo"),
-//           List("Foo", "Foo", "Foo"),
-//           List("Foo", "Foo", "Foo"),
-//           List("Foo", "Foo", "Foo"),
-//           List("Foo", "Foo", "Foo")
-//         )
+class AdvancedTests extends FreeSpec {
 
-//         @ case class Foo(i: Int, s0: String, s1: Seq[String])
-//         defined class Foo
+  def check = new TestRepl()
 
-//         @ Foo(1, "", Nil)
-//         res2: ${sessionPrefix}Foo = Foo(1, "", List())
+  "pprint" in {
+    check.session(s"""
+        @ Seq.fill(10)(Seq.fill(3)("Foo"))
+        res0: Seq[Seq[String]] = List(
+          List("Foo", "Foo", "Foo"),
+          List("Foo", "Foo", "Foo"),
+          List("Foo", "Foo", "Foo"),
+          List("Foo", "Foo", "Foo"),
+          List("Foo", "Foo", "Foo"),
+          List("Foo", "Foo", "Foo"),
+          List("Foo", "Foo", "Foo"),
+          List("Foo", "Foo", "Foo"),
+          List("Foo", "Foo", "Foo"),
+          List("Foo", "Foo", "Foo")
+        )
 
-//         @ Foo(
-//         @   1234567,
-//         @   "I am a cow, hear me moo",
-//         @   Seq("I weigh twice as much as you", "and I look good on the barbecue")
-//         @ )
-//         res3: ${sessionPrefix}Foo = Foo(
-//           1234567,
-//           "I am a cow, hear me moo",
-//           List("I weigh twice as much as you", "and I look good on the barbecue")
-//         )
-//       """)
-//     }
+        @ case class Foo(i: Int, s0: String, s1: Seq[String])
+        defined class Foo
 
-//     'exit {
-//       check.result("exit", Res.Exit())
-//     }
-//     'skip {
-//       check.result("", Res.Skip)
-//     }
+        @ Foo(1, "", Nil)
+        res2: ${sessionPrefix}Foo = Foo(1, "", List())
 
-//     'predef {
-//       val check2 = new TestRepl {
-//         override def predef = """
-//           import math.abs
-//           val x = 1
-//           val y = "2"
-//         """
-//       }
-//       check2.session("""
-//         @ -x
-//         res0: Int = -1
+        @ Foo(
+        @   1234567,
+        @   "I am a cow, hear me moo",
+        @   Seq("I weigh twice as much as you", "and I look good on the barbecue")
+        @ )
+        res3: ${sessionPrefix}Foo = Foo(
+          1234567,
+          "I am a cow, hear me moo",
+          List("I weigh twice as much as you", "and I look good on the barbecue")
+        )
+      """)
+  }
 
-//         @ y
-//         res1: String = "2"
+  "exit" in {
+    check.result("exit", Res.Exit())
+  }
 
-//         @ x + y
-//         res2: String = "12"
+  "skip" in {
+    check.result("", Res.Skip)
+  }
 
-//         @ abs(-x)
-//         res3: Int = 1
-//       """)
+  "predef" in {
+    val check2 = new TestRepl {
+      override def predef = """
+          import math.abs
+          val x = 1
+          val y = "2"
+        """
+    }
+    check2.session("""
+        @ -x
+        res0: Int = -1
 
-//     }
-//     'predefSettings {
-//       val check2 = new TestRepl {
-//         override def predef = """
-//           repl.compiler.settings.Xexperimental.value = true
-//         """
-//       }
-//       check2.session("""
-//         @ repl.compiler.settings.Xexperimental.value
-//         res0: Boolean = true
-//       """)
+        @ y
+        res1: String = "2"
 
-//     }
-//     'macros {
-//       check.session("""
-//         @ import language.experimental.macros
+        @ x + y
+        res2: String = "12"
 
-//         @ import reflect.macros.Context
+        @ abs(-x)
+        res3: Int = 1
+      """)
 
-//         @ def impl(c: Context): c.Expr[String] = {
-//         @  import c.universe._
-//         @  c.Expr[String](Literal(Constant("Hello!")))
-//         @ }
-//         defined function impl
+  }
 
-//         @ def m: String = macro impl
-//         defined function m
+  "predefSettings" in {
+    val check2 = new TestRepl {
+      override def predef = """
+          repl.compiler.settings.Xexperimental.value = true
+        """
+    }
+    check2.session("""
+        @ repl.compiler.settings.Xexperimental.value
+        res0: Boolean = true
+      """)
 
-//         @ m
-//         res4: String = "Hello!"
-//       """)
-//     }
-//     'typeScope {
-//       // Fancy type-printing isn't implemented at all in 2.10.x
-//       if (!scala2_10)
-//         check.session("""
-//         @ collection.mutable.Buffer(1)
-//         res0: collection.mutable.Buffer[Int] = ArrayBuffer(1)
+  }
 
-//         @ import collection.mutable
+  "macros" in {
+    check.session("""
+        @ import language.experimental.macros
 
-//         @ collection.mutable.Buffer(1)
-//         res2: mutable.Buffer[Int] = ArrayBuffer(1)
+        @ import reflect.macros.Context
 
-//         @ mutable.Buffer(1)
-//         res3: mutable.Buffer[Int] = ArrayBuffer(1)
+        @ def impl(c: Context): c.Expr[String] = {
+        @  import c.universe._
+        @  c.Expr[String](Literal(Constant("Hello!")))
+        @ }
+        defined function impl
 
-//         @ import collection.mutable.Buffer
+        @ def m: String = macro impl
+        defined function m
 
-//         @ mutable.Buffer(1)
-//         res5: Buffer[Int] = ArrayBuffer(1)
-//       """)
-//     }
-//     'customTypePrinter {
-//       check.session("""
-//         @ Array(1)
-//         res0: Array[Int] = Array(1)
+        @ m
+        res4: String = "Hello!"
+      """)
+  }
 
-//         @ import pprint.TPrint
+  "typeScope" in {
+    check.session("""
+        @ collection.mutable.Buffer(1)
+        res0: collection.mutable.Buffer[Int] = ArrayBuffer(1)
 
-//         @ implicit def ArrayTPrint[T: TPrint]: TPrint[Array[T]] = TPrint.lambda( c =>
-//         @   implicitly[TPrint[T]].render(c) +
-//         @   " " +
-//         @   c.typeColor("Array").render
-//         @ )
+        @ import collection.mutable
 
-//         @ Array(1)
-//         res3: Int Array = Array(1)
-//       """)
-//     }
-//     'unwrapping {
-//       check.session("""
-//         @ {
-//         @   val x = 1
-//         @   val y = 2
-//         @   x + y
-//         @ }
-//         x: Int = 1
-//         y: Int = 2
-//         res0_2: Int = 3
-//       """)
-//     }
-//     'forceWrapping {
-//       check.session("""
-//         @ {{
-//         @   val x = 1
-//         @   val y = 2
-//         @   x + y
-//         @ }}
-//         res0: Int = 3
-//       """)
-//     }
-//     'truncation {
-//       // Need a way to capture stdout in tests to make these tests work
-//       if (false)
-//         check.session("""
-//         @ Seq.fill(20)(100)
-//         res0: Seq[Int] = List(
-//           100,
-//           100,
-//           100,
-//           100,
-//           100,
-//           100,
-//           100,
-//           100,
-//           100,
-//           100,
-//           100,
-//           100,
-//           100,
-//           100,
-//         ...
+        @ collection.mutable.Buffer(1)
+        res2: mutable.Buffer[Int] = ArrayBuffer(1)
 
-//         @ show(Seq.fill(20)(100))
-//         res1: ammonite.pprint.Show[Seq[Int]] = List(
-//           100,
-//           100,
-//           100,
-//           100,
-//           100,
-//           100,
-//           100,
-//           100,
-//           100,
-//           100,
-//           100,
-//           100,
-//           100,
-//           100,
-//           100,
-//           100,
-//           100,
-//           100,
-//           100,
-//           100
-//         )
+        @ mutable.Buffer(1)
+        res3: mutable.Buffer[Int] = ArrayBuffer(1)
 
-//         @ show(Seq.fill(20)(100), height = 3)
-//         res2: ammonite.pprint.Show[Seq[Int]] = List(
-//           100,
-//           100,
-//         ...
+        @ import collection.mutable.Buffer
 
-//         @ pprintConfig() = pprintConfig().copy(height = 5 )
+        @ mutable.Buffer(1)
+        res5: Buffer[Int] = ArrayBuffer(1)
+      """)
+  }
 
-//         @ Seq.fill(20)(100)
-//         res4: Seq[Int] = List(
-//           100,
-//           100,
-//           100,
-//           100,
-//         ...
-//       """)
-//     }
-//     'private {
-//       check.session("""
-//         @ private val x = 1; val y = x + 1
-//         x: Int = 1
-//         y: Int = 2
+  "customTypePrinter" in {
+    check.session("""
+        @ Array(1)
+        res0: Array[Int] = Array(1)
 
-//         @ y
-//         res1: Int = 2
+        @ import pprint.TPrint
 
-//         @ x
-//         error: not found: value x
-//       """)
-//     }
-//     'compilerPlugin - retry(3) {
-//       check.session("""
-//         @ // Make sure plugins from eval class loader are not loaded
+        @ implicit def ArrayTPrint[T: TPrint]: TPrint[Array[T]] = TPrint.lambda( c =>
+        @   implicitly[TPrint[T]].render(c) +
+        @   " " +
+        @   c.typeColor("Array").render
+        @ )
 
-//         @ import $ivy.`org.spire-math::kind-projector:0.6.3`
+        @ Array(1)
+        res3: Int Array = Array(1)
+      """)
+  }
 
-//         @ trait TC0[F[_]]
-//         defined trait TC0
+  "unwrapping" in {
+    check.session("""
+        @ {
+        @   val x = 1
+        @   val y = 2
+        @   x + y
+        @ }
+        x: Int = 1
+        y: Int = 2
+        res0_2: Int = 3
+      """)
+  }
 
-//         @ type TC0EitherStr = TC0[Either[String, ?]]
-//         error: not found: type ?
+  "forceWrapping" in {
+    check.session("""
+        @ {{
+        @   val x = 1
+        @   val y = 2
+        @   x + y
+        @ }}
+        res0: Int = 3
+      """)
+  }
 
-//         @ // This one must be loaded
+  // 'truncation {
+  //   // Need a way to capture stdout in tests to make these tests work
+  //   if (false)
+  //     check.session("""
+  //       @ Seq.fill(20)(100)
+  //       res0: Seq[Int] = List(
+  //         100,
+  //         100,
+  //         100,
+  //         100,
+  //         100,
+  //         100,
+  //         100,
+  //         100,
+  //         100,
+  //         100,
+  //         100,
+  //         100,
+  //         100,
+  //         100,
+  //       ...
 
-//         @ import $plugin.$ivy.`org.spire-math::kind-projector:0.6.3`
+  //       @ show(Seq.fill(20)(100))
+  //       res1: ammonite.pprint.Show[Seq[Int]] = List(
+  //         100,
+  //         100,
+  //         100,
+  //         100,
+  //         100,
+  //         100,
+  //         100,
+  //         100,
+  //         100,
+  //         100,
+  //         100,
+  //         100,
+  //         100,
+  //         100,
+  //         100,
+  //         100,
+  //         100,
+  //         100,
+  //         100,
+  //         100
+  //       )
 
-//         @ trait TC[F[_]]
-//         defined trait TC
+  //       @ show(Seq.fill(20)(100), height = 3)
+  //       res2: ammonite.pprint.Show[Seq[Int]] = List(
+  //         100,
+  //         100,
+  //       ...
 
-//         @ type TCEitherStr = TC[Either[String, ?]]
-//         defined type TCEitherStr
+  //       @ pprintConfig() = pprintConfig().copy(height = 5 )
 
-//         @ // Useless - does not add plugins, and ignored by eval class loader
+  //       @ Seq.fill(20)(100)
+  //       res4: Seq[Int] = List(
+  //         100,
+  //         100,
+  //         100,
+  //         100,
+  //       ...
+  //     """)
+  // }
 
-//         @ import $plugin.$ivy.`com.lihaoyi::scalatags:0.4.5`
+  "private" in {
+    check.session("""
+        @ private val x = 1; val y = x + 1
+        x: Int = 1
+        y: Int = 2
 
-//         @ import scalatags.Text
-//         error: not found: value scalatags
-//       """)
-//     }
-//     'desugar {
-//       if (!scala2_10)
-//         check.session("""
-//         @ desugar{1 + 2 max 3}
-//         res0: Desugared = scala.Predef.intWrapper(3).max(3)
-//       """)
-//     }
-//     'loadingModulesInPredef {
-//       val dir = pwd / 'src / 'test / 'resources / 'scripts / 'predefWithLoad
-//       'loadExec {
-//         val c1 = new TestRepl() {
-//           override def predef = read ! dir / "PredefLoadExec.sc"
-//         }
-//         c1.session("""
-//           @ val previouslyLoaded = predefDefinedValue
-//           previouslyLoaded: Int = 1337
-//         """)
-//       }
-//       'loadModule {
-//         val c2 = new TestRepl() {
-//           override def predef = read ! dir / "PredefLoadModule.sc"
-//         }
-//         c2.session("""
-//           @ val previouslyLoaded = 1337
-//           previouslyLoaded: Int = 1337
-//         """)
-//       }
-//     }
-//   }
-// }
+        @ y
+        res1: Int = 2
+
+        @ x
+        error: not found: value x
+      """)
+  }
+
+  "compilerPlugin" in {
+    check.session("""
+        @ // Make sure plugins from eval class loader are not loaded
+
+        @ import $ivy.`org.spire-math::kind-projector:0.6.3`
+
+        @ trait TC0[F[_]]
+        defined trait TC0
+
+        @ type TC0EitherStr = TC0[Either[String, ?]]
+        error: not found: type ?
+
+        @ // This one must be loaded
+
+        @ import $plugin.$ivy.`org.spire-math::kind-projector:0.6.3`
+
+        @ trait TC[F[_]]
+        defined trait TC
+
+        @ type TCEitherStr = TC[Either[String, ?]]
+        defined type TCEitherStr
+
+        @ // Useless - does not add plugins, and ignored by eval class loader
+
+        @ import $plugin.$ivy.`com.lihaoyi::scalatags:0.4.5`
+
+        @ import scalatags.Text
+        error: not found: value scalatags
+      """)
+  }
+
+  "desugar" in {
+    check.session("""
+        @ desugar{1 + 2 max 3}
+        res0: Desugared = scala.Predef.intWrapper(3).max(3)
+      """)
+  }
+
+  "loadingModulesInPredef" - {
+    val dir = pwd / 'src / 'test / 'resources / 'scripts / 'predefWithLoad
+    "loadExec"  in {
+      val c1 = new TestRepl() {
+        override def predef = read ! dir / "PredefLoadExec.sc"
+      }
+      c1.session("""
+            @ val previouslyLoaded = predefDefinedValue
+            previouslyLoaded: Int = 1337
+          """)
+    }
+    "loadModule" in {
+      val c2 = new TestRepl() {
+        override def predef = read ! dir / "PredefLoadModule.sc"
+      }
+      c2.session("""
+            @ val previouslyLoaded = 1337
+            previouslyLoaded: Int = 1337
+          """)
+    }
+  }
+
+}
