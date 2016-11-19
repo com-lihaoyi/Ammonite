1391a72fcac06b673c0f04f72951db3d95cace42
Li Haoyi <haoyi.sg@gmail.com>
2016-11-19 21:50:20 +0800

    cleanup

--- amm/interp/src/main/scala-2.11/ammonite/interp/CompilerCompatibility.scala
+++ amm/interp/src/main/scala-2.11/ammonite/interp/CompilerCompatibility.scala
@@ -1,21 +1,11 @@
 package ammonite.runtime
 
-import ammonite.util.ImportData
 
-import scala.reflect.internal.util.Position
-import scala.reflect.io
-import scala.reflect.io.{AbstractFile, FileZipArchive, VirtualDirectory}
-import scala.tools.nsc.classpath._
-import scala.tools.nsc.{Global, Settings}
-import scala.tools.nsc.interactive.{Global => InteractiveGlobal}
+import scala.tools.nsc.Global
+import scala.tools.nsc.interactive.{ Global => InteractiveGlobal }
 import scala.tools.nsc.plugins.Plugin
-import scala.tools.nsc.reporters.AbstractReporter
 import scala.tools.nsc.typechecker.Analyzer
-import scala.tools.nsc.util.ClassPath.JavaContext
-import scala.tools.nsc.util._
-import scala.reflect.io._
-import scala.tools.nsc
-import scala.tools.nsc.backend.JavaPlatform
+
 object CompilerCompatibility {
   def analyzer(g: Global, cl: ClassLoader): Analyzer { val global: g.type } =
     new { val global: g.type = g } with Analyzer {
