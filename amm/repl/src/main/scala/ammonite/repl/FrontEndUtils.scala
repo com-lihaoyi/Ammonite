c849b92518d076018f86d1184c15ad20636b0101
Piotr Kwiecinski <piokwiecinski@gmail.com>
2017-12-17 17:29:47 +0000

    Remove bash and tput from terminal dependencies

--- amm/repl/src/main/scala/ammonite/repl/FrontEndUtils.scala
+++ amm/repl/src/main/scala/ammonite/repl/FrontEndUtils.scala
@@ -6,8 +6,8 @@ import ammonite.util.Util.newLine
  * Created by haoyi on 8/29/15.
  */
 object FrontEndUtils {
-  def width = ammonite.terminal.TTY.consoleDim("cols")
-  def height = ammonite.terminal.TTY.consoleDim("lines")
+  def width: Int = ammonite.terminal.TTY.consoleWidth
+  def height: Int = ammonite.terminal.TTY.consoleHeight
   def tabulate(snippetsRaw: Seq[fansi.Str], width: Int): Iterator[String] = {
     val gap = 2
     val snippets = if (snippetsRaw.isEmpty) Seq(fansi.Str("")) else snippetsRaw
@@ -27,9 +27,9 @@ object FrontEndUtils {
   }
 
   @tailrec def findPrefix(strings: Seq[String], i: Int = 0): String = {
-    if (strings.count(_.length > i) == 0) strings(0).take(i)
+    if (strings.count(_.length > i) == 0) strings.head.take(i)
     else if(strings.collect{ case x if x.length > i => x(i)}.distinct.length > 1)
-      strings(0).take(i)
+      strings.head.take(i)
     else findPrefix(strings, i + 1)
   }
 
@@ -37,19 +37,17 @@ object FrontEndUtils {
                        details: Seq[String]): List[String] = {
 
     val prelude =
-      if (details.length != 0 || completions.length != 0) List(newLine)
+      if (details.nonEmpty || completions.nonEmpty) List(newLine)
       else Nil
 
     val detailsText =
-      if (details.length == 0) Nil
+      if (details.isEmpty) Nil
       else FrontEndUtils.tabulate(details.map(fansi.Str(_)), FrontEndUtils.width)
 
-
     val completionText =
-      if (completions.length == 0) Nil
+      if (completions.isEmpty) Nil
       else FrontEndUtils.tabulate(completions.map(fansi.Str(_)), FrontEndUtils.width)
 
     prelude ++ detailsText ++ completionText
   }
-
 }
