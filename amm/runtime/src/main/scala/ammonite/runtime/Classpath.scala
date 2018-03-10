50ecce4d0e83ba5a58b91cf489f56c675d5a7f54
Robby <robby@k-state.edu>
2018-03-10 07:12:52 -0600

    Cleaned up formatting.

--- amm/runtime/src/main/scala/ammonite/runtime/Classpath.scala
+++ amm/runtime/src/main/scala/ammonite/runtime/Classpath.scala
@@ -8,21 +8,21 @@ import io.github.retronym.java9rtexport.Export
 import scala.util.control.NonFatal
 
 /**
-  * Loads the jars that make up the classpath of the scala-js-fiddle
-  * compiler and re-shapes it into the correct structure to satisfy
-  * scala-compile and scalajs-tools
-  */
+ * Loads the jars that make up the classpath of the scala-js-fiddle
+ * compiler and re-shapes it into the correct structure to satisfy
+ * scala-compile and scalajs-tools
+ */
 object Classpath {
   val traceClasspathIssues =
     sys.props
-      .get("ammonite.trace-classpath")
-      .exists(_.toLowerCase == "true")
+       .get("ammonite.trace-classpath")
+       .exists(_.toLowerCase == "true")
 
   /**
-    * In memory cache of all the jars used in the compiler. This takes up some
-    * memory but is better than reaching all over the filesystem every time we
-    * want to do something.
-    */
+   * In memory cache of all the jars used in the compiler. This takes up some
+   * memory but is better than reaching all over the filesystem every time we
+   * want to do something.
+   */
   var current = Thread.currentThread().getContextClassLoader
   val files = collection.mutable.Buffer.empty[java.io.File];
   {
