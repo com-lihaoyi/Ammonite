705464e8810ee52d6452c98870ceeb27af6a0d26
Jonathan Keatley <jckeatley@gmail.com>
2018-07-12 14:24:19 -0500

    Removed the Cygwin code from readme, because it's built in now.

--- readme/Sample.scala
+++ readme/Sample.scala
@@ -19,7 +19,6 @@ object Sample{
     ") > /usr/local/bin/amm && chmod +x /usr/local/bin/amm' && amm"
   val replCurl = curlCommand(ammonite.Constants.curlUrl)
   val unstableCurl = curlCommand(ammonite.Constants.unstableCurlUrl)
-  val cygwinSed = """$ sed -i '0,/"\$0"/{s/"\$0"/`cygpath -w "\$0"`/}' /usr/local/bin/amm"""
   val filesystemCurl =
     "$ mkdir -p ~/.ammonite && curl -L -o ~/.ammonite/predef.sc https://git.io/vHaKQ"
   val cacheVersion = 6
