6ceb37b8c9c82d12f183034dee30218d07835d03
Li Haoyi <haoyi.sg@gmail.com>
2018-10-13 22:16:21 +0800

    Highlighting works again

--- amm/repl/src/main/scala/ammonite/repl/Highlighter.scala
+++ amm/repl/src/main/scala/ammonite/repl/Highlighter.scala
@@ -3,7 +3,7 @@ package ammonite.repl
 
 import ammonite.interp.Parsers
 
-import fastparse._
+import fastparse._, NoWhitespace._
 
 import scalaparse.Scala._
 import scalaparse.syntax.Identifiers._
@@ -84,38 +84,44 @@ object Highlighter {
       var indices = collection.mutable.Buffer((0, endColor))
       var done = false
       val input = buffer.mkString
+      val stack = collection.mutable.ArrayBuffer.empty[(T, Int, Int)]
       parse(input, parser, instrument = new ParsingRun.Instrument {
-        def beforeParse(parser: String, index: Int): Unit = ()
-        def afterParse(parser: String, index: Int, success: Boolean): Unit = ()
-      }/*(rule, idx, res) => {
-        for(color <- ruleColors.lift(rule)){
-          val closeColor = indices.last._2
-          val startIndex = indices.length
-          indices += ((idx, color))
+        def beforeParse(parser: String, index: Int): Unit = {
+          for(color <- ruleColors.lift(parser)) {
+            val closeColor = indices.last._2
+            val startIndex = indices.length
+            indices += ((index, color))
 
-          res() match {
-            case s: Parsed.Success[_] =>
+            stack.append((closeColor, startIndex, index))
+          }
+        }
+        def afterParse(parser: String, index: Int, success: Boolean): Unit = {
+          for(color <- ruleColors.lift(parser)) {
+            val (closeColor, startIndex, idx) = stack.remove(stack.length - 1)
+
+            def endCheckParser[_: P] = P(WL ~ End)
+
+            if (success) {
               val prev = indices(startIndex - 1)._1
 
-              if (idx < prev && s.index <= prev){
+              if (idx < prev && index <= prev) {
                 indices.remove(startIndex, indices.length - startIndex)
 
               }
-              while (idx < indices.last._1 && s.index <= indices.last._1) {
+              while (idx < indices.last._1 && index <= indices.last._1) {
                 indices.remove(indices.length - 1)
               }
-              indices += ((s.index, closeColor))
-              if (s.index == buffer.length) done = true
-            case f: Parsed.Failure
-              if f.index == buffer.length
-              && (WL ~ End).parse(input, idx).isInstanceOf[Parsed.Failure] =>
-              // EOF, stop all further parsing
+              indices += ((index, closeColor))
+              if (index == buffer.length) done = true
+            } else if (index == buffer.length && !parse(input, endCheckParser(_), index).isSuccess) {
               done = true
-            case _ =>  // hard failure, or parsed nothing. Discard all progress
+            } else {
               indices.remove(startIndex, indices.length - startIndex)
+            }
           }
         }
-      }*/)
+      })
+
       indices
     }
     // Make sure there's an index right at the start and right at the end! This
