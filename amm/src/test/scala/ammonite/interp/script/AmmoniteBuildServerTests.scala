253d3969a865662b4ce26ef4a658abeee362882f
Alexandre Archambault <alexandre.archambault@gmail.com>
2020-05-04 12:52:27 +0200

    Fix position update for first line in script

--- amm/src/test/scala/ammonite/interp/script/AmmoniteBuildServerTests.scala
+++ amm/src/test/scala/ammonite/interp/script/AmmoniteBuildServerTests.scala
@@ -11,6 +11,7 @@ import scala.collection.JavaConverters._
 import scala.compat.java8.FutureConverters
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
+import scala.meta.internal.semanticdb.TextDocument
 
 object AmmoniteBuildServerTests extends TestSuite {
 
@@ -318,53 +319,102 @@ object AmmoniteBuildServerTests extends TestSuite {
 
     "semanticdb" - {
       val scriptPath = os.RelPath("semdb/main.sc")
-      val runner = new BspScriptRunner(wd / scriptPath)
+      val otherScriptPath = os.RelPath("semdb/other.sc")
+      val runner = new BspScriptRunner(wd / scriptPath, wd / otherScriptPath)
 
       val expectedClassPath = List(
         s"com/softwaremill/sttp/client/core_$sbv/2.0.6/core_$sbv-2.0.6.jar",
         s"com/softwaremill/sttp/model/core_$sbv/1.0.2/core_$sbv-1.0.2.jar"
       ).map("https/repo1.maven.org/maven2/" + _)
 
+      val otherScriptUri = (wd / otherScriptPath).toNIO.toUri.toASCIIString
+
+      def semanticDb(
+        scalacOptionsItem: ScalacOptionsItem,
+        scriptPath: os.RelPath
+      ): TextDocument = {
+        import scala.meta.internal.semanticdb._
+
+        val classDirectory = os.Path(Paths.get(new URI(scalacOptionsItem.getClassDirectory)))
+        val semanticDbPath = classDirectory /
+          "META-INF" /
+          "semanticdb" /
+          scriptPath.segments.init /
+          s"${scriptPath.segments.last}.semanticdb"
+        assert(os.isFile(semanticDbPath))
+
+        val docs = TextDocuments.parseFrom(os.read.bytes(semanticDbPath))
+        val doc = docs.documents match {
+          case Seq(doc0) => doc0
+          case _ =>
+            throw new Exception(
+              s"Invalid number of text documents in semanticdb: " +
+                docs.documents.length
+            )
+        }
+
+        val scriptContent = os.read(wd / scriptPath)
+        assert(doc.text == scriptContent)
+
+        doc
+      }
+
+      def checkMainScript(scalacOptionsItem: ScalacOptionsItem, scriptPath: os.RelPath): Unit = {
+        import scala.meta.internal.semanticdb._
+
+        val doc = semanticDb(scalacOptionsItem, scriptPath)
+
+        val quickRequestOccurrence = new SymbolOccurrence(
+          Some(new Range(3, 0, 3, 12)),
+          "sttp/client/SttpApi#quickRequest.",
+          SymbolOccurrence.Role.REFERENCE
+        )
+
+        assert(doc.occurrences.contains(quickRequestOccurrence))
+      }
+
+      def checkOtherScript(scalacOptionsItem: ScalacOptionsItem, scriptPath: os.RelPath): Unit = {
+        import scala.meta.internal.semanticdb._
+
+        val doc = semanticDb(scalacOptionsItem, scriptPath)
+
+        val listOccurrence = new SymbolOccurrence(
+          Some(new Range(0, 11, 0, 15)),
+          "scala/collection/immutable/List.",
+          SymbolOccurrence.Role.REFERENCE
+        )
+
+        assert(doc.occurrences.contains(listOccurrence))
+      }
+
       for {
-        List(scalacOptionsItem) <- runner.init()
+        scalacOptionsItems <- runner.init()
+        scalacOptionsItem = scalacOptionsItems
+          .find(_.getTarget == runner.expectedBuildTargetId)
+          .getOrElse(
+            throw new Exception(
+              "scalac options item not found for " +
+                runner.expectedBuildTargetId
+            )
+          )
+        otherScalacOptionsItem = scalacOptionsItems
+          .find(_.getTarget.getUri == otherScriptUri)
+          .getOrElse(
+            throw new Exception(
+              "scalac options item not found for " +
+                runner.expectedBuildTargetId
+            )
+          )
 
         _ <- runner.compile(StatusCode.OK)
 
         diagnostics = runner.diagnostics()
         _ = assert(diagnostics.isEmpty)
 
-        _ = {
-          import scala.meta.internal.semanticdb._
+        _ = checkMainScript(scalacOptionsItem, scriptPath)
 
-          val classDirectory = os.Path(Paths.get(new URI(scalacOptionsItem.getClassDirectory)))
-          val semanticDbPath = classDirectory /
-            "META-INF" /
-            "semanticdb" /
-            scriptPath.segments.init /
-            s"${scriptPath.segments.last}.semanticdb"
-          assert(os.isFile(semanticDbPath))
-
-          val docs = TextDocuments.parseFrom(os.read.bytes(semanticDbPath))
-          val doc = docs.documents match {
-            case Seq(doc0) => doc0
-            case _ =>
-              throw new Exception(
-                s"Invalid number of text documents in semanticdb: " +
-                  docs.documents.length
-              )
-          }
-
-          val scriptContent = os.read(wd / scriptPath)
-          assert(doc.text == scriptContent)
-
-          val quickRequestOccurrence = new SymbolOccurrence(
-            Some(new Range(3, 0, 3, 12)),
-            "sttp/client/SttpApi#quickRequest.",
-            SymbolOccurrence.Role.REFERENCE
-          )
-
-          assert(doc.occurrences.contains(quickRequestOccurrence))
-        }
+        _ <- runner.compile(StatusCode.OK, otherScriptUri)
+        _ = checkOtherScript(otherScalacOptionsItem, otherScriptPath)
 
       } yield ()
     }
