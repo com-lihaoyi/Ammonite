5d0c0976325167dfa79ee06f166c889f22b79cee
Alexandre Archambault <alexarchambault@users.noreply.github.com>
2020-06-26 15:07:29 +0200

    Add scala 2.13.3 support (#1098)

--- amm/interp/src/main/scala/ammonite/interp/script/AmmoniteBuildServer.scala
+++ amm/interp/src/main/scala/ammonite/interp/script/AmmoniteBuildServer.scala
@@ -64,7 +64,7 @@ class AmmoniteBuildServer(
           Dependency.of(
             "org.scalameta",
             "semanticdb-scalac_" + scala.util.Properties.versionNumberString,
-            "4.3.9"
+            "4.3.17"
           )
         ),
         root,
