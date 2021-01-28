b047b399dfc2a90a8db8a6c567ea59fef8612cb4
Alexandre Archambault <alexandre.archambault@gmail.com>
2021-01-28 15:45:12 +0100

    Disable YRangeposTests in Scala 3
    No -Yrangepos option to tinker with in Scala 3 I think.

--- amm/src/test/scala/ammonite/interp/YRangeposTests.scala
+++ amm/src/test/scala/ammonite/interp/YRangeposTests.scala
@@ -19,7 +19,7 @@ object YRangeposTests extends TestSuite {
     val scriptFolderPath =
       os.pwd / 'amm / 'src / 'test / 'resources / 'scriptCompilerSettings
 
-    test("Yrangepos"){
+    def simpleTest() = {
       // This tests shows that enabling Yrangepos does not mess with ammonite's
       // behaviour. The compiler not crashing is the test itself.
       val storage = Storage.InMemory()
@@ -30,14 +30,22 @@ object YRangeposTests extends TestSuite {
       val res = Scripts.runScript(os.pwd, scriptFolderPath / "yRangepos.sc", interp)
       assert(res.isSuccess)
     }
+    test("Yrangepos"){
+      if (scala2) simpleTest()
+      else "Disabled in Scala 3"
+    }
 
-    test("YrangeposError"){
+    def errorTest() = {
       // This tests shows that enabling Yrangepos does not mess with ammonite's
       // behaviour, by checking that the line at which the error is found matches
       // the expected one in the file
       val expectedErrorMessage = "yRangeposError.sc:9: type mismatch;"
       checkErrorMessage(os.rel / 'scriptCompilerSettings / "yRangeposError.sc",
         expectedErrorMessage)
+    }
+    test("YrangeposError"){
+      if (scala2) errorTest()
+      else "Disabled in Scala 3"
     }
   }
 }
