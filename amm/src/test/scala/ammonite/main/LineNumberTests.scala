8d88a460d6647b1f11a9ae28a8da0940a4383008
Alexandre Archambault <alexandre.archambault@gmail.com>
2021-02-03 20:58:21 +0100

    Adjust LineNumberTests for Scala 3

--- amm/src/test/scala/ammonite/main/LineNumberTests.scala
+++ amm/src/test/scala/ammonite/main/LineNumberTests.scala
@@ -19,6 +19,9 @@ object LineNumberTests extends TestSuite{
       assert(e.err.contains(expected))
     }
 
+    val sv = ammonite.compiler.CompilerBuilder.scalaVersion
+    val isScala2 = sv.startsWith("2.")
+
     //All Syntax Error tests currently don't pass on windows as fastparse gives out some 10
     //surrounding chars which are different on windows and linux due to `\n` and `\r\n`
     //as `\r\n` counts as 2 so less number of surrounding chars are shown on windows
@@ -27,9 +30,16 @@ object LineNumberTests extends TestSuite{
         checkErrorMessage(
           file = os.rel / 'lineNumbers / "ErrorLineNumberTest.sc",
           expected = Util.normalizeNewlines(
-            """ErrorLineNumberTest.sc:5:24 expected "}"
-              |    printlnqs(unsorted))
-              |                       ^""".stripMargin
+            if (isScala2)
+              """ErrorLineNumberTest.sc:5:24 expected "}"
+                |    printlnqs(unsorted))
+                |                       ^""".stripMargin
+            else
+              """ErrorLineNumberTest.sc
+                |  |    printlnqs(unsorted))
+                |  |                       ^
+                |  |                       '}' expected, but ')' found
+                |""".stripMargin
           )
         )
       }
@@ -40,9 +50,16 @@ object LineNumberTests extends TestSuite{
         checkErrorMessage(
           file = os.rel / 'lineNumbers/"MultipleCompilationUnitErrorMsgTest1.sc",
           expected = Util.normalizeNewlines(
-            """MultipleCompilationUnitErrorMsgTest1.sc:5:1 expected end-of-input
-              |}
-              |^""".stripMargin
+            if (isScala2)
+              """MultipleCompilationUnitErrorMsgTest1.sc:5:1 expected end-of-input
+                |}
+                |^""".stripMargin
+            else
+              """MultipleCompilationUnitErrorMsgTest1.sc
+                |  |}
+                |  |^
+                |  |eof expected, but '}' found
+                |""".stripMargin
           )
         )
       }
@@ -54,9 +71,16 @@ object LineNumberTests extends TestSuite{
         checkErrorMessage(
           file = os.rel / 'lineNumbers/"MultipleCompilationUnitErrorMsgTest2.sc",
           expected = Util.normalizeNewlines(
-            """MultipleCompilationUnitErrorMsgTest2.sc:3:1 expected end-of-input
-              |}
-              |^""".stripMargin
+            if (isScala2)
+              """MultipleCompilationUnitErrorMsgTest2.sc:3:1 expected end-of-input
+                |}
+                |^""".stripMargin
+            else
+              """MultipleCompilationUnitErrorMsgTest2.sc
+                |  |}
+                |  |^
+                |  |eof expected, but '}' found
+                |""".stripMargin
           )
         )
       }
@@ -65,41 +89,68 @@ object LineNumberTests extends TestSuite{
     test("compilationErrorWithCommentsAtTop") - checkErrorMessage(
       file = os.rel / 'lineNumbers/"compilationErrorWithCommentsAtTop.sc",
       expected = Util.normalizeNewlines(
-        """compilationErrorWithCommentsAtTop.sc:11: not found: value quicort
-          |    quicort(unsorted.filter(_ < pivot)):::List(pivot):::""".stripMargin +
-        """quicksort(unsorted.filter(_ > pivot))"""
+        if (isScala2)
+          """compilationErrorWithCommentsAtTop.sc:11: not found: value quicort
+            |    quicort(unsorted.filter(_ < pivot)):::List(pivot):::""".stripMargin +
+          """quicksort(unsorted.filter(_ > pivot))"""
+        else {
+          val firstLine = "quicort(unsorted.filter(_ < pivot)):::List(pivot):::" +
+            "quicksort(unsorted.filter(_ > pivot))"
+          s"""|   |    $firstLine
+              |   |    ^^^^^^^
+              |   |    Not found: quicort""".stripMargin
+        }
       )
     )
 
     test("compilationErrorInSecondBlock") - checkErrorMessage(
       file = os.rel / 'lineNumbers/"compilationErrorInSecondBlock.sc",
       expected = Util.normalizeNewlines(
-        """compilationErrorInSecondBlock.sc:14: not found: value printnl
-          |val res_0 = printnl("OK")
-          |            ^""".stripMargin
+        if (isScala2)
+          """compilationErrorInSecondBlock.sc:14: not found: value printnl
+            |val res_0 = printnl("OK")
+            |            ^""".stripMargin
+        else
+          """   |val res_0 = printnl("OK")
+            |   |            ^^^^^^^
+            |   |            Not found: printnl""".stripMargin
       )
     )
 
     test("compilationErrorInFourthBlock") - checkErrorMessage(
       file = os.rel / 'lineNumbers/"compilationErrorInFourthBlock.sc",
       expected = Util.normalizeNewlines(
-        """compilationErrorInFourthBlock.sc:30: not found: value prinntl
-          |val res = prinntl("Ammonite")
-          |          ^""".stripMargin
+        if (isScala2)
+          """compilationErrorInFourthBlock.sc:30: not found: value prinntl
+            |val res = prinntl("Ammonite")
+            |          ^""".stripMargin
+        else
+          """   |val res = prinntl("Ammonite")
+            |   |          ^^^^^^^
+            |   |          Not found: prinntl""".stripMargin
       )
     )
 
     test("compilationErrorInClass") - checkErrorMessage(
       file = os.rel / 'lineNumbers/"compilationErrorInClass.sc",
-      expected = "compilationErrorInClass.sc:17: value a is not a member of"
+      expected =
+        if (isScala2)
+          "compilationErrorInClass.sc:17: value a is not a member of"
+        else
+          "value a cannot be accessed as a member of"
     )
 
     test("CompilationErrorLineNumberTest") - checkErrorMessage(
       file = os.rel / 'lineNumbers/"CompilationErrorLineNumberTest.sc",
       expected = Util.normalizeNewlines(
-        """CompilationErrorLineNumberTest.sc:7: not found: value noSuchObject
-          |  val x = noSuchObject.badFunction
-          |          ^""".stripMargin
+        if (isScala2)
+          """CompilationErrorLineNumberTest.sc:7: not found: value noSuchObject
+            |  val x = noSuchObject.badFunction
+            |          ^""".stripMargin
+        else
+          """   |  val x = noSuchObject.badFunction
+            |   |          ^^^^^^^^^^^^
+            |   |          Not found: noSuchObject""".stripMargin
       )
     )
 
