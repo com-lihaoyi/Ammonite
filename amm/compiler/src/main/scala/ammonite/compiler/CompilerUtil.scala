2de458c2249f1c983d2e2b22dd3f1497d85bd168
Alex Archambault <alexandre.archambault@gmail.com>
2023-07-05 14:46:26 +0200

    Ignore more symbols in imports
    With warnings enabled, importing those prints many warnings like
    ```
    imported `notifyAll' is permanently hidden by definition of method notifyAll in class Object
    ```

--- amm/compiler/src/main/scala/ammonite/compiler/CompilerUtil.scala
+++ amm/compiler/src/main/scala/ammonite/compiler/CompilerUtil.scala
@@ -14,6 +14,20 @@ object CompilerUtil {
     "$main",
     // Don't care about this
     "toString",
+    "equals",
+    "wait",
+    "notify",
+    "notifyAll",
+    "synchronized",
+    "hashCode",
+    "getClass",
+    "eq",
+    "ne",
+    "##",
+    "==",
+    "!=",
+    "isInstanceOf",
+    "asInstanceOf",
     // Behaves weird in 2.10.x, better to just ignore.
     "_"
   )
