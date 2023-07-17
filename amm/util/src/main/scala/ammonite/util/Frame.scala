74c8477d7644625fc119dba3a6250fc9d6326516
Alexandre Archambault <alexarchambault@users.noreply.github.com>
2023-07-17 14:36:07 +0200

    Allow users to register dependency hooks (#1357)
    Called every time JARs are added to the class path

--- amm/util/src/main/scala/ammonite/util/Frame.scala
+++ amm/util/src/main/scala/ammonite/util/Frame.scala
@@ -8,4 +8,15 @@ trait Frame {
 
   def classpath: Seq[URL]
   def version: Int
+
+  /** Adds a [[Frame.Hook]] to be called every time JARs are added to the class path */
+  def addHook(hook: Frame.Hook): Unit
+}
+
+object Frame {
+  /** A hook that can be called every time JARs are added to the class path */
+  trait Hook {
+    /** Called when new JARs are added to the class path */
+    def addClasspath(additional: Seq[java.net.URL]): Unit
+  }
 }
