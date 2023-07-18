com-lihaoyi/Ammonite

commit 94c5ce87f3508590d347c2bb2372fb4e9d7a5cf6

Fix line numbers in exceptions for multi-line code blocks (#1361)
## What is changed?
The current Parser ignores whitespaces at the beginning of the code
block, which may result in incorrect line number in an exception
stackTrace:
```
@ {
  
  
  1 / 0
  } 
java.lang.ArithmeticException: / by zero
  ammonite.$sess.cmd0$.<init>(cmd0.sc:1)
  ammonite.$sess.cmd0$.<clinit>(cmd0.sc)
```

This PR changes it so the result will be
```
@ { 
  
  
  1 / 0
  } 
java.lang.ArithmeticException: / by zero
  ammonite.$sess.cmd0$.<init>(cmd0.sc:4)
  ammonite.$sess.cmd0$.<clinit>(cmd0.sc)

```
(line number is 4 instead of 1)

This it done by introducing a new parser which does not ignore leading
whitespaces


Please note that it changes this only for Scala 2. Scala 3 impl has a
different parsing logics which I didn't have time to dive into yet
## How is it tested?
New tests in `FailureTests` checking line numbers

amm/compiler/src/main/scala-2/ammonite/compiler/Parsers.scala
@@ -63,8 +63,11 @@ object Parsers extends IParser {
   private def StatementBlock[_: P](blockSep: => P0) =
     P( Semis.? ~ (Index ~ (!blockSep ~ TmplStat ~~ WS ~~ (Semis | &("}") | End)).!).repX)
 
-  private def Splitter0[_: P] = P( StatementBlock(Fail) )
-  def Splitter[_: P] = P( ("{" ~ Splitter0 ~ "}" | Splitter0) ~ End )
+  private def Splitter0[_: P] = P(StatementBlock(Fail))
+
+  private def HighlightSplitter[_: P] = P( ("{" ~ Splitter0 ~ "}" | Splitter0) ~ End )
+
+  def Splitter[_: P] = P(("{" ~~ WL.! ~~ Splitter0 ~ "}" | WL.! ~~ Splitter0) ~ End)
 
   private def ObjParser[_: P] = P( ObjDef )
 
@@ -94,14 +97,29 @@ object Parsers extends IParser {
         case f @ Parsed.Failure(_, _, _) => Some(Left(
           formatFastparseError(fileName, code, f)
         ))
-        case Parsed.Success(value, index) => Some(Right(value.map(_._2)))
+        case Parsed.Success(value, index) => {
+          val (str, seq) = value
+          if (seq.isEmpty) {
+            Some(Right(Nil))
+          } else {
+            Some(Right(Seq(str + seq.head._2) ++ seq.tail.map(_._2)))
+          }
+        }
       }
     } else
       parse(code, Splitter(_)) match{
         case f @ Parsed.Failure(_, _, _) => Some(Left(
           formatFastparseError(fileName, code, f)
         ))
-        case Parsed.Success(value, index) => Some(Right(value.map(_._2)))
+        case Parsed.Success(value, index) => {
+          val (str, seq) = value
+          if (seq.isEmpty) {
+            Some(Right(Nil))
+          } else {
+            Some(Right(Seq(str + seq.head._2) ++ seq.tail.map(_._2)))
+          }
+
+        }
       }
 
   def isObjDef(code: String): Boolean = {
@@ -265,7 +283,7 @@ object Parsers extends IParser {
                        keyword: fansi.Attrs,
                        notImplemented: fansi.Attrs,
                        reset: fansi.Attrs) = {
-    Highlighter.defaultHighlight0(Splitter(_), buffer, comment, `type`, literal, keyword, reset)
+    Highlighter.defaultHighlight0(HighlightSplitter(_), buffer, comment, `type`, literal, keyword, reset)
   }
   def defaultHighlightIndices(buffer: Vector[Char],
                               comment: fansi.Attrs,
@@ -273,11 +291,11 @@ object Parsers extends IParser {
                               literal: fansi.Attrs,
                               keyword: fansi.Attrs,
                               reset: fansi.Attrs) = Highlighter.defaultHighlightIndices0(
-    Splitter(_), buffer, comment, `type`, literal, keyword, reset
+    HighlightSplitter(_), buffer, comment, `type`, literal, keyword, reset
   )
 
   def highlightIndices[T](buffer: Vector[Char],
                           ruleColors: PartialFunction[String, T],
                           endColor: T): Seq[(Int, T)] =
-    Highlighter.highlightIndices(Parsers.Splitter(_), buffer, ruleColors, endColor)
+    Highlighter.highlightIndices(Parsers.HighlightSplitter(_), buffer, ruleColors, endColor)
 }

amm/repl/src/test/scala/ammonite/session/FailureTests.scala
@@ -77,5 +77,36 @@ object FailureTests extends TestSuite{
         !x.contains("Something unexpected went wrong =(")
       )
     }
+
+    test("lineNumbersInStackTrace1") {
+      if (check.scala2) {
+        check.fail(
+          """
+            |
+            |
+            | 1 / 0
+            |""".stripMargin, x =>
+            x.contains("/ by zero") &&
+            x.contains("cmd0.sc:4") // check that the line number is correct
+
+        )
+      }
+    }
+
+    test("lineNumbersInStackTrace2") {
+      if (check.scala2) {
+        check.fail(
+        """
+          |{
+          |
+          | // block command
+          | 1 / 0
+          |}
+          |""".stripMargin, x =>
+          x.contains("/ by zero") &&
+          x.contains("cmd0.sc:5") // check that the line number is correct
+        )
+      }
+    }
   }
 }

commit 151446c55e763b3b8c0152226f617ad1a0d719d4

Fix recent regression with import hooks in blocks (#1369)
This fixes a regression introduced in
https://github.com/com-lihaoyi/Ammonite/pull/1361. This makes the
following work again as expected:
```scala
@ {
    import $ivy.`org.typelevel::cats-kernel:2.6.1`
  }
```

Before the changes here, in Scala 2.x, one gets an error like
```text
cmd0.sc:2: object org.typelevel::cats-kernel:2.6.1 is not a member of package ammonite.$ivy
import $ivy.`org.typelevel::cats-kernel:2.6.1`
       ^
Compilation Failed
```

amm/compiler/src/main/scala-2/ammonite/compiler/Parsers.scala
@@ -39,7 +39,7 @@ object Parsers extends IParser {
   }
 
   def ImportSplitter[_: P]: P[Seq[ammonite.util.ImportTree]] =
-    P( `import` ~/ ImportExpr.rep(1, sep = ","./) )
+    P( WL ~ `import` ~/ ImportExpr.rep(1, sep = ","./) )
 
   def ImportFinder[_: P]: P[String] =
     P(WL ~ `import` ~/ ImportExpr.! ~ End)

amm/repl/src/test/scala/ammonite/session/AdvancedTests.scala
@@ -708,6 +708,19 @@ object AdvancedTests extends TestSuite{
         """
       )
     }
+    test("hook in block") {
+      check.session(
+        """
+          @ {
+          @   import $ivy.`org.typelevel::cats-kernel:2.6.1`
+          @ }
+          import $ivy.$
+
+          @ import cats.kernel._
+          import cats.kernel._
+        """
+      )
+    }
     test("class-path-hook") {
       val sbv = check.scalaBinaryVersion
       check.session(